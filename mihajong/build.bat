2d080704e08c3d70a809981f03b935105bf84884
TANIGAWA Takashi <mihailjp@gmail.com>
2012-01-15 09:45:12 +0900

    ビルド用のバッチファイル(試作)

---
+++ mihajong/build.bat
@@ -0,0 +1,10 @@
+@echo off
+rem 
+rem 自動ビルド用スクリプト
+rem 
+rem 環境変数 %HSPPATH%に、予めHSPのパスを設定の上、
+rem cHspCompをインストールしてください。
+rem http://lldev.jp/others/freeware.html#chspcomp
+rem 
+
+for %%i in ( mihajong.hsp mihasanm.hsp mihaysnm.hsp mihassnm.hsp ) do %HSPPATH%\chspcomp.exe /m %CD%\%%i
