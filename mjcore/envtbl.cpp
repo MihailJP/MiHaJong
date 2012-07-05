9432f14939641d74a1c701050726a3b9d5d5bf4d
TANIGAWA Takashi <mihailjp@gmail.com>
2012-07-06 00:40:06 +0900

    プレイヤー名などの情報を格納した構造体を移植

---
+++ mjcore/envtbl.cpp
@@ -0,0 +1,67 @@
+#include "envtbl.h"
+
+EnvTable* EnvTable::Instantiate() {
+	// Singleton instance accessor
+	static EnvTable instance;
+	return &instance;
+}
+
+/* HSPからのアクセサ */
+
+__declspec(dllexport) void EnvTable::setPlayerName(void *, int PlayerNum, const char* const NameStr) {
+	Instantiate()->PlayerDat[PlayerNum].PlayerName = NameStr;
+}
+__declspec(dllexport) void EnvTable::getPlayerName(void *, int PlayerNum, char* const NameStr, int bufsize) {
+	strcpy_s(NameStr, bufsize, Instantiate()->PlayerDat[PlayerNum].PlayerName.c_str());
+}
+
+__declspec(dllexport) void EnvTable::setRemotePlayer(void *, int PlayerNum, int Value) {
+	Instantiate()->PlayerDat[PlayerNum].RemotePlayerFlag = Value;
+}
+__declspec(dllexport) int EnvTable::isRemotePlayer(void *, int PlayerNum) {
+	return Instantiate()->PlayerDat[PlayerNum].RemotePlayerFlag ? 1 : 0;
+}
+
+__declspec(dllexport) void EnvTable::setGameMode(void *, int Value) {
+	Instantiate()->GameMode = (ClientType)Value;
+}
+__declspec(dllexport) int EnvTable::getGameMode(void *) {
+	return (int)Instantiate()->GameMode;
+}
+
+__declspec(dllexport) void EnvTable::setWatchModeFlag(void *, int Value) {
+	Instantiate()->WatchModeFlag = Value;
+}
+__declspec(dllexport) int EnvTable::getWatchModeFlag(void *) {
+	return (int)Instantiate()->WatchModeFlag;
+}
+
+__declspec(dllexport) void EnvTable::setTableColor(void *, int R, int G, int B) {
+	Instantiate()->bgColorR = R;
+	Instantiate()->bgColorG = G;
+	Instantiate()->bgColorB = B;
+}
+__declspec(dllexport) int EnvTable::getTableColorR(void *) {
+	return (int)(Instantiate()->bgColorR);
+}
+__declspec(dllexport) int EnvTable::getTableColorG(void *) {
+	return (int)(Instantiate()->bgColorG);
+}
+__declspec(dllexport) int EnvTable::getTableColorB(void *) {
+	return (int)(Instantiate()->bgColorB);
+}
+
+__declspec(dllexport) void EnvTable::TableEnvInit(void *) {
+	for (int i = 0; i < PLAYERS; i++) {
+		Instantiate()->PlayerDat[i].PlayerName = "";
+		Instantiate()->PlayerDat[i].RemotePlayerFlag = false;
+	}
+	Instantiate()->GameMode = Unavailable;
+	Instantiate()->WatchModeFlag = false;
+}
+__declspec(dllexport) void EnvTable::TableEnvInitAll(void *) {
+	TableEnvInit(NULL);
+	Instantiate()->bgColorR =
+		Instantiate()->bgColorG =
+		Instantiate()->bgColorB = 0;
+}
---
+++ mjcore/envtbl.h
@@ -0,0 +1,50 @@
+#ifndef ENVTBL_H
+#define ENVTBL_H
+
+#include <cstdint>
+#include <string>
+#include "gametbl.h"
+
+class EnvTable { // 卓には直接関係ないデータ [singleton]
+private: // singleton
+	EnvTable(){}
+	EnvTable(const EnvTable& eTbl);
+	EnvTable& operator=(const EnvTable& eTbl);
+
+	// 内部クラスとかを定義する
+	class PlayerLabel {
+	public:
+		std::string PlayerName;
+		bool RemotePlayerFlag;
+	};
+	enum ClientType : int32_t {
+		Unavailable = (-1),
+		Standalone, Server, Client,
+	};
+public:
+	static EnvTable* Instantiate(); // Singleton instance accessor
+
+	// データ
+	InfoByPlayer<PlayerLabel> PlayerDat;
+	ClientType GameMode;
+	bool WatchModeFlag;
+	uint8_t bgColorR, bgColorG, bgColorB;
+
+	// HSPからのアクセサ
+	__declspec(dllexport) static void setPlayerName(void *, int PlayerNum, const char* const NameStr);
+	__declspec(dllexport) static void getPlayerName(void *, int PlayerNum, char* const NameStr, int bufsize);
+	__declspec(dllexport) static void setRemotePlayer(void *, int PlayerNum, int Value);
+	__declspec(dllexport) static int isRemotePlayer(void *, int PlayerNum);
+	__declspec(dllexport) static void setGameMode(void *, int Value);
+	__declspec(dllexport) static int getGameMode(void *);
+	__declspec(dllexport) static void setWatchModeFlag(void *, int Value);
+	__declspec(dllexport) static int getWatchModeFlag(void *);
+	__declspec(dllexport) static void setTableColor(void *, int R, int G, int B);
+	__declspec(dllexport) static int getTableColorR(void *);
+	__declspec(dllexport) static int getTableColorG(void *);
+	__declspec(dllexport) static int getTableColorB(void *);
+	__declspec(dllexport) static void TableEnvInit(void *);
+	__declspec(dllexport) static void TableEnvInitAll(void *);
+};
+
+#endif
