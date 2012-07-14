ce296b17165a1362361bae7618b4ce7393a14b04
TANIGAWA Takashi <mihailjp@gmail.com>
2012-07-14 21:57:06 +0900

    ソースコードアーカイブ用のバッチファイルをC++コードにも対応させる

--- archive_source.bat
+++ archive_source.bat
@@ -4,9 +4,25 @@ mkdir release
 mkdir release\source
 del /q mihajong\_*.hsp
 copy mihajong\*.hsp release\source
-copy mihajong\*.as release\source
+
 mkdir release\source\icons
 copy mihajong\icons\*.ico release\source\icons
+
+mkdir release\source\mjcore
+copy mjcore\*.cpp release\source\mjcore
+copy mjcore\*.h release\source\mjcore
+copy mjcore\*.rc release\source\mjcore
+copy mjcore\*.sln release\source\mjcore
+copy mjcore\*.vcxproj release\source\mjcore
+copy mjcore\*.vcxproj.filters release\source\mjcore
+
+mkdir release\source\logger
+copy logger\*.cpp release\source\logger
+copy logger\*.h release\source\logger
+copy logger\*.rc release\source\logger
+copy logger\*.vcxproj release\source\logger
+copy logger\*.vcxproj.filters release\source\logger
+
 del /q source.zip
 cd release
 "C:\Program Files\7-zip\7z.exe" a -mx=9 ..\source.zip source
