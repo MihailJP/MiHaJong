4a61635cbd5aa5be3bf63836201467fcf4462bfc
TANIGAWA Takashi <mihailjp@gmail.com>
2012-07-14 19:43:42 +0900

    バージョン情報を追加

---
+++ mjcore/version.h
@@ -0,0 +1,19 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define MIHAJONG_MAJOR_VER 1
+#define MIHAJONG_MINOR_VER 6
+#define MIHAJONG_PATCH_VER 0
+#define MIHAJONG_VER "1.6.0"
+
+#define MJCORE_MAJOR_VER 1
+#define MJCORE_MINOR_VER 6
+#define MJCORE_PATCH_VER 0
+#define MJCORE_VER "1.6.0"
+
+#define LOGGER_MAJOR_VER 1
+#define LOGGER_MINOR_VER 6
+#define LOGGER_PATCH_VER 0
+#define LOGGER_VER "1.6.0"
+
+#endif
