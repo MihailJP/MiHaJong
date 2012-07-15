2d851bc5e72f7f57470e620c3d51cf7db0a5aa88
TANIGAWA Takashi <mihailjp@gmail.com>
2012-07-15 20:51:50 +0900

    月相を計算するルーチンを再移植

--- astro/astro.h
+++ astro/astro.h
@@ -12,8 +12,13 @@
 
 #ifndef rad2deg
 #include <math.h>
-#define rad2deg(deg) ((deg) * 180.0 / M_PI)
+#define rad2deg(rad) ((rad) * 180.0 / M_PI)
 #endif /* rad2deg */
+
+#ifndef fixangle
+#include <math.h>
+#define fixangle(deg) fmod(deg, 360.0)
+#endif /* fixangle */
 
 #else /* ASTRO_EXPORTS */
 #define EXPORT __declspec(dllimport)
@@ -25,6 +30,17 @@
 
 #include <Windows.h>
 
+struct MoonPhase
+{
+	double MoonPhase; /* the terminator phase angle as a percentage of a full circle (i.e., 0 to 1) */
+	double MoonIllum; /* the illuminated fraction of the Moon's disc */
+	double MoonAge; /* the Moon's age in days and fraction */
+	double MoonDist; /* the distance of the Moon from the centre of the Earth */
+	double MoonAng; /* the angular diameter subtended by the Moon as seen by an observer at the centre of the Earth. */
+	double SunDist; /* the distance from the Sun in km */
+	double SunAng; /* the angular size of Sun in degrees */
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -34,8 +50,12 @@ extern "C" {
 	EXPORT void time_to_julian_hsp(double*, int, int, int, int, int, int, int);
 
 	/* ecllong.c */
-	EXPORT double sun_ecliptic_longitude (double jday);
-	EXPORT void sun_ecliptic_longitude_hsp (double* l, double jday);
+	EXPORT double sun_ecliptic_longitude (double);
+	EXPORT void sun_ecliptic_longitude_hsp (double*, double);
+
+	/* mphase.c */
+	EXPORT struct MoonPhase calc_moon_phase (double);
+	EXPORT void calc_moon_phase_hsp (double*, double);
 
 #ifdef __cplusplus
 }
--- astro/ecllong.c
+++ astro/ecllong.c
@@ -9,7 +9,7 @@ EXPORT double sun_ecliptic_longitude (double jday)
 	M = deg2rad(357.5256 + 35999.049 * T);
 	L = O + M + (6191.2 * T + 6893.0 * sin(M) + 72.0 * sin(2.0 * M))
 		/ 1296000.0 * 2.0 * M_PI;
-	return fmod(rad2deg(L), 360.0);
+	return fixangle(rad2deg(L));
 }
 
 EXPORT void sun_ecliptic_longitude_hsp (double* l, double jday)
