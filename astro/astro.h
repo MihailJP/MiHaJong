47a9cac9ff52e7818c507d5256930ef524714f61
TANIGAWA Takashi <mihailjp@gmail.com>
2012-07-15 18:59:59 +0900

    太陽黄経を求めるルーチンを移植

--- astro/astro.h
+++ astro/astro.h
@@ -4,13 +4,41 @@
 #ifdef ASTRO_EXPORTS
 #pragma once
 #define EXPORT __declspec(dllexport)
-#else
+
+#ifndef deg2rad
+#include <math.h>
+#define deg2rad(deg) ((deg) * M_PI / 180.0)
+#endif /* deg2rad */
+
+#ifndef rad2deg
+#include <math.h>
+#define rad2deg(deg) ((deg) * 180.0 / M_PI)
+#endif /* rad2deg */
+
+#else /* ASTRO_EXPORTS */
 #define EXPORT __declspec(dllimport)
-#endif
+#endif /* ASTRO_EXPORTS */
+
+#ifdef _MSC_VER
+#define _USE_MATH_DEFINES
+#endif /* _MSC_VER */
 
 #include <Windows.h>
 
-/* julian.c */
-EXPORT double systime_to_julian(const LPSYSTEMTIME);
-EXPORT void time_to_julian_hsp(double*, int, int, int, int, int, int, int);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	/* julian.c */
+	EXPORT double systime_to_julian(const LPSYSTEMTIME);
+	EXPORT void time_to_julian_hsp(double*, int, int, int, int, int, int, int);
+
+	/* ecllong.c */
+	EXPORT double sun_ecliptic_longitude (double jday);
+	EXPORT void sun_ecliptic_longitude_hsp (double* l, double jday);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
