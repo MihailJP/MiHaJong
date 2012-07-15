70db4e867eda3299faa82399e89c9f6abe5790ba
TANIGAWA Takashi <mihailjp@gmail.com>
2012-07-15 18:26:06 +0900

    ユリウス日を求める処理を書く

--- astro/astro.h
+++ astro/astro.h
@@ -2,9 +2,15 @@
 #define ASTRO_H
 
 #ifdef ASTRO_EXPORTS
+#pragma once
 #define EXPORT __declspec(dllexport)
 #else
 #define EXPORT __declspec(dllimport)
 #endif
 
+#include <Windows.h>
+
+/* julian.c */
+EXPORT double systime_to_julian(const LPSYSTEMTIME);
+EXPORT void time_to_julian_hsp(double*, int, int, int, int, int, int, int);
 #endif
