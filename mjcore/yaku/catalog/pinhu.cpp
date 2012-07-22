18e788edcf25b954b165db563039ec25874cbb1c
TANIGAWA Takashi <mihailjp@gmail.com>
2012-07-22 18:36:04 +0900

    余計なYが混ざっていたので直す

--- mjcore/yaku/catalog/pinhu.cpp
+++ mjcore/yaku/catalog/pinhu.cpp
@@ -3,7 +3,7 @@
 void yaku::yakuCalculator::YakuCatalog::catalogInit::yakulst_pinhu()
 {
 	yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
-		"平和", Yyaku::yakuCalculator::Yaku::yval_1han /* 門前であるかどうかの判定は既に行なっているので不要 */,
+		"平和", yaku::yakuCalculator::Yaku::yval_1han /* 門前であるかどうかの判定は既に行なっているので不要 */,
 		[](const GameTable* const, const MENTSU_ANALYSIS* const analysis) {
 			return analysis->isPinfu;
 		}
