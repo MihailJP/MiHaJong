09a916364426c7038912169db641af504cba9aac
TANIGAWA Takashi <mihailjp@gmail.com>
2012-07-30 13:16:12 +0900

    一気通貫系の役(途中まで)

--- mjcore/yaku/catalog/sequence.cpp
+++ mjcore/yaku/catalog/sequence.cpp
@@ -123,4 +123,202 @@ void yaku::yakuCalculator::YakuCatalog::catalogInit::yakulst_sequence() {
 				return (yakuFlagCount == 2);
 			}
 		));
+
+	// ---------------------------------------------------------------------
+
+	/* サンショクともサンシキとも呼ばれる */
+	yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+		"三色同順",yaku::yakuCalculator::Yaku::yval_2han_kuisagari,
+		[](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+			bool yakuFlag = false;
+			for (int i = 1; i <= 7; i++)
+				if ((analysis->ShunziCount[i + TILE_SUIT_CHARACTERS] >= 1) &&
+					(analysis->ShunziCount[i + TILE_SUIT_CIRCLES] >= 1) &&
+					(analysis->ShunziCount[i + TILE_SUIT_BAMBOOS] >= 1)) yakuFlag = true;
+			return yakuFlag;
+		}
+	));
+
+	// ---------------------------------------------------------------------
+
+	/* イーチートンカン */
+	auto ikki_tsuukan =
+		[](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis,
+		bool* const yakuFlag, int* const yakuCol) -> void {
+			*yakuFlag = false;
+			for (int i = 0; i < TILE_SUIT_HONORS; i += TILE_SUIT_STEP) {
+				if ((analysis->ShunziCount[i + 1] >= 1) &&
+					(analysis->ShunziCount[i + 4] >= 1) &&
+					(analysis->ShunziCount[i + 7] >= 1)) {
+						*yakuFlag = true;
+						if (yakuCol != NULL) *yakuCol = i;
+				}
+			}
+		};
+	yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+		"一気通貫",yaku::yakuCalculator::Yaku::yval_2han_kuisagari,
+			[ikki_tsuukan](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+				bool yakuFlag; ikki_tsuukan(gameStat, analysis, &yakuFlag, NULL);
+				return (yakuFlag && // 一気通貫が成立していて
+					(analysis->DuiziCount[EastWind] >= 1) && // 東と
+					(analysis->DuiziCount[NorthWind] >= 1) ); // 北がある
+			}
+	));
+	/* 20101204祝東京～新青森全通 */
+	if (getRule(RULE_TOHOKU_SHINKANSEN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"東北新幹線", (getRule(RULE_TOHOKU_SHINKANSEN) == 1) ?
+			(yaku::yakuCalculator::Yaku::HANFUNC)yaku::yakuCalculator::Yaku::yval_yakuman :
+			(yaku::yakuCalculator::Yaku::HANFUNC)yaku::yakuCalculator::Yaku::yval_yakuman_menzen,
+			"一気通貫", "混一色",
+			[ikki_tsuukan](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+				bool yakuFlag; ikki_tsuukan(gameStat, analysis, &yakuFlag, NULL);
+				return (yakuFlag && // 一気通貫が成立していて
+					(analysis->DuiziCount[EastWind] >= 1) && // 東と
+					(analysis->DuiziCount[NorthWind] >= 1) ); // 北がある
+			}
+		));
+	/* 一気通貫全帯幺九 */
+	if (getRule(RULE_ITTSUU_CHANTA) != 0) {
+		auto ittsuu_chanta_counting =
+			[](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis,
+			int* const yaojiu, int* const zipai) -> void {
+				*yaojiu = *zipai = 0;
+				for (int i = 0; i < SIZE_OF_MELD_BUFFER; i++) {
+					switch (analysis->MianziDat[i].tile) {
+					case EastWind: case SouthWind: case WestWind: case NorthWind:
+					case WhiteDragon: case GreenDragon: case RedDragon:
+						++*zipai;
+						/* FALLTHRU */
+					case CharacterOne: case CharacterNine: case CircleOne: case CircleNine:
+					case BambooOne: case BambooNine:
+						++*yaojiu; break;
+					case CharacterSeven: case CircleSeven: case BambooSeven:
+						if ((i > 0) && (analysis->MianziDat[i].mstat < meldTripletConcealed))
+							++*yaojiu;
+						break;
+					}
+				}
+			};
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"一気通貫全帯幺九", yaku::yakuCalculator::Yaku::yval_3han_kuisagari,
+			"一気通貫",
+			[ittsuu_chanta_counting](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+				int yaojiu, zipai;
+				ittsuu_chanta_counting(gameStat, analysis, &yaojiu, &zipai);
+				return ((yaojiu == 4) && (zipai > 0));
+			}
+		));
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"純一気通貫全帯幺九", yaku::yakuCalculator::Yaku::yval_4han_kuisagari,
+			"一気通貫",
+			[ittsuu_chanta_counting](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+				int yaojiu, zipai;
+				ittsuu_chanta_counting(gameStat, analysis, &yaojiu, &zipai);
+				return ((yaojiu == 4) && (zipai == 0));
+			}
+		));
+	}
+	/* 五心通貫 */
+	if (getRule(RULE_UUSHINTONKAN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"五心通貫", yaku::yakuCalculator::Yaku::yval_1han,
+			[ikki_tsuukan](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+				bool yakuFlag; int yakuCol = 0;
+				ikki_tsuukan(gameStat, analysis, &yakuFlag, &yakuCol);
+				return (yakuFlag && // 一気通貫が成立していて
+					(analysis->Machi == yaku::yakuCalculator::machiKanchan) && // 嵌張待ちで
+					(gameStat->Player[analysis->player].Hand[NUM_OF_TILES_IN_HAND].tile == (yakuCol + 5)) // 和了牌が5
+					);
+			}
+		));
+	/* 竜戯珠 */
+	if (getRule(RULE_LONGXIZHU) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"竜戯珠", yaku::yakuCalculator::Yaku::yval_2han,
+			[ikki_tsuukan](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+				bool yakuFlag; ikki_tsuukan(gameStat, analysis, &yakuFlag, NULL);
+				return (yakuFlag && // 一気通貫が成立していて
+					(analysis->KeziCount[WestWind] >= 1) && // 西の刻子があり
+					(analysis->MianziDat[0].tile == CircleOne) ); // アタマが一筒
+			}
+		));
+	/* 金龍戯北斗 */
+	if (getRule(RULE_JINLONG_DOU_BEIDOU) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"金龍戯北斗", yaku::yakuCalculator::Yaku::yval_2han,
+			[ikki_tsuukan](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+				bool yakuFlag; ikki_tsuukan(gameStat, analysis, &yakuFlag, NULL);
+				return (yakuFlag && // 一気通貫が成立していて
+					(analysis->KeziCount[CircleSeven] >= 1) && // 七筒の刻子があり
+					(analysis->MianziDat[0].tile == NorthWind) ); // アタマが北
+			}
+		));
+	/* 西湖十景系列の役 */
+	{
+		const int Rules = 10;
+		const RuleCode tmpRuleCodeList[Rules] = {
+			RULE_SHUANGFENG_ZHAYUN, RULE_NANBING_WANZHONG, RULE_LEIFENG_XIZHAO, RULE_LIULANG_WENYING, RULE_DUANQIAO_CANXUE,
+			RULE_SUDI_CHUNXIAO, RULE_HUAGANG_GUANYU, RULE_SANTAN_YINYUE, RULE_QUYUAN_FENGHE, RULE_PINGHU_QIUYUE,
+		};
+		const tileCode tmpTileCodeList[Rules][2] = {
+			{EastWind, BambooEight}, {SouthWind, RedDragon}, {WestWind, CircleEight}, {NorthWind, CharacterEight},
+			{WhiteDragon, BambooTwo}, {GreenDragon, CharacterTwo}, {RedDragon, CircleTwo},
+			{WhiteDragon, CharacterOne}, {GreenDragon, CircleOne}, {RedDragon, BambooOne},
+		};
+		const int tmpStraightColorList[Rules] = {
+			TILE_SUIT_CHARACTERS, TILE_SUIT_CIRCLES, TILE_SUIT_BAMBOOS, TILE_SUIT_CIRCLES, TILE_SUIT_CHARACTERS,
+			TILE_SUIT_CIRCLES, TILE_SUIT_BAMBOOS, TILE_SUIT_CHARACTERS, TILE_SUIT_CIRCLES, TILE_SUIT_BAMBOOS,
+		};
+		const char tmpYakuNameList[Rules][16] = {
+			"双峰挿雲", "南屏晩鍾", "雷峰夕照", "柳浪聞鶯", "断橋残雪",
+			"蘇堤春暁", "花港観魚", "三譚印月", "曲院風荷", "平湖秋月",
+		};
+		for (int i = 0; i < Rules; i++) {
+			if (getRule(tmpRuleCodeList[i]) == 0) continue;
+			tileCode tmpTileCode1 = tmpTileCodeList[i][0];
+			tileCode tmpTileCode2 = tmpTileCodeList[i][1];
+			int tmpStraightColor = tmpStraightColorList[i];
+			yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+				tmpYakuNameList[i], yaku::yakuCalculator::Yaku::yval_2han_kuisagari,
+				[ikki_tsuukan, tmpTileCode1, tmpTileCode2, tmpStraightColor]
+				(const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+					bool yakuFlag; int yakuCol;
+					ikki_tsuukan(gameStat, analysis, &yakuFlag, &yakuCol);
+					return (yakuFlag && // 一気通貫が成立していて
+						(analysis->DuiziCount[tmpTileCode1] >= 1) && // 指定の牌が刻子と雀頭として存在し
+						(analysis->DuiziCount[tmpTileCode2] >= 1) &&
+						(yakuCol == tmpStraightColor) ); // イッツー部分が指定された色
+				}
+			));
+		}
+	}
+	/* 東の青龍 */
+	if (getRule(RULE_AZURE_DRAGON) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"東の青龍", yaku::yakuCalculator::Yaku::yval_yakuman_menzen,
+			"一気通貫", "混一色",
+			[ikki_tsuukan](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+				bool yakuFlag; int yakuCol;
+				ikki_tsuukan(gameStat, analysis, &yakuFlag, &yakuCol);
+				return (yakuFlag && // 一気通貫が成立していて
+					(analysis->KeziCount[EastWind] >= 1) && // 東の刻子があり
+					(analysis->MianziDat[0].tile == WhiteDragon) && // アタマが白で
+					(yakuCol == TILE_SUIT_CIRCLES) ); // イッツー部分が筒子
+			}
+		));
+	/* 北の玄武 */
+	if (getRule(RULE_BLACK_TORTOISE) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"北の玄武", yaku::yakuCalculator::Yaku::yval_yakuman_menzen,
+			"一気通貫",
+			[ikki_tsuukan](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+				bool yakuFlag; int yakuCol;
+				ikki_tsuukan(gameStat, analysis, &yakuFlag, &yakuCol);
+				return (yakuFlag && // 一気通貫が成立していて
+					(analysis->KeziCount[NorthWind] >= 1) && // 北の刻子があり
+					(analysis->MianziDat[0].tile == BambooSeven) && // アタマが七索で
+					(yakuCol == TILE_SUIT_CIRCLES) ); // イッツー部分が筒子
+			}
+		));
 }
