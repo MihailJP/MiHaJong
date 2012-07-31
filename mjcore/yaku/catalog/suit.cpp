1e19e2aa86d5517c08cc3460fad53098adc5b6e5
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-01 03:10:42 +0900

    九連宝燈を再実装

--- mjcore/yaku/catalog/suit.cpp
+++ mjcore/yaku/catalog/suit.cpp
@@ -22,8 +22,45 @@ void yaku::yakuCalculator::YakuCatalog::catalogInit::yakulst_suit() {
 			return isshoku(analysis, true);
 		}
 	));
-	/* ここにいろいろチンイツの上位役 */
-
+	/* 和了ったら死ぬ */
+	auto chkHaishiki =
+		[](const MENTSU_ANALYSIS* const analysis, const char* const haishiki) -> bool {
+			bool yakuFlag = false;
+			for (int i = 0; i < TILE_SUIT_HONORS; i += TILE_SUIT_STEP)
+				if ((analysis->TileCount[i + 1] >= (int)(haishiki[0] - '0')) &&
+					(analysis->TileCount[i + 2] >= (int)(haishiki[1] - '0')) &&
+					(analysis->TileCount[i + 3] >= (int)(haishiki[2] - '0')) &&
+					(analysis->TileCount[i + 4] >= (int)(haishiki[3] - '0')) &&
+					(analysis->TileCount[i + 5] >= (int)(haishiki[4] - '0')) &&
+					(analysis->TileCount[i + 6] >= (int)(haishiki[5] - '0')) &&
+					(analysis->TileCount[i + 7] >= (int)(haishiki[6] - '0')) &&
+					(analysis->TileCount[i + 8] >= (int)(haishiki[7] - '0')) &&
+					(analysis->TileCount[i + 9] >= (int)(haishiki[8] - '0')) ) yakuFlag = true;
+			return yakuFlag;
+		};
+	yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+		"九蓮宝燈", yaku::yakuCalculator::Yaku::yval_yakuman_menzen,
+		"清一色",
+		[chkHaishiki](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+			return chkHaishiki(analysis, "311111113");
+		}
+	));
+	if (getRule(RULE_DOUBLE_YAKUMAN) == 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"純正九蓮宝燈", yaku::yakuCalculator::Yaku::yval_double_yakuman_menzen,
+			"九蓮宝燈", "清一色",
+			[chkHaishiki](const GameTable* const gameStat, const MENTSU_ANALYSIS* const analysis) -> bool {
+				bool yakuFlag = false;
+				const tileCode* tsumoTile = &(gameStat->Player[analysis->player].Hand[NUM_OF_TILES_IN_HAND - 1].tile);
+				if ((*tsumoTile % 10 == 1) || (*tsumoTile % 10 == 9)) {
+					if (analysis->TileCount[*tsumoTile] >= 4) yakuFlag = true;
+				} else {
+					if (analysis->TileCount[*tsumoTile] >= 2) yakuFlag = true;
+				}
+				return (chkHaishiki(analysis, "311111113")) && (!analysis->MachiInfo.FuritenFlag);
+			}
+		));
+	/* TODO: その他のチンイツの上位役 */
 	/* ホンイツ */
 	yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
 		"混一色", yaku::yakuCalculator::Yaku::yval_3han_kuisagari,
