f0c04741b86e6356789c8541b1c288874c1ece1d
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-03 03:14:31 +0900

    銅鑼和

--- mjcore/yaku/catalog/dora.cpp
+++ mjcore/yaku/catalog/dora.cpp
@@ -1,4 +1,14 @@
 #include "../catalog.h"
 
 void yaku::yakuCalculator::YakuCatalog::catalogInit::yakulst_dora() {
+	/* ドラで和了ったら1飜 */
+	if (getRule(RULE_DORAHOH) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"銅鑼和", yaku::yakuCalculator::Yaku::yval_1han,
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return ((analysis->shanten[shantenAll] == -1) && // 何かの手で和了になっている
+					((analysis->GameStat->DoraFlag.Omote[analysis->TsumoHai->tile] > 0) || // ツモ牌が表ドラになっている(裏ドラは対象外)
+					(analysis->TsumoHai->red != Normal))); // 赤ドラか青ドラになっている
+			}
+		));
 }
