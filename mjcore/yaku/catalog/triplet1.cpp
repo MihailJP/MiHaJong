43d52089782c9ee0d1b9e81379970c817f430638
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-04 04:48:49 +0900

    四暗刻、三暗刻

--- mjcore/yaku/catalog/triplet1.cpp
+++ mjcore/yaku/catalog/triplet1.cpp
@@ -1,4 +1,36 @@
 #include "../catalog.h"
 
 void yaku::yakuCalculator::YakuCatalog::catalogInit::yakulst_triplet_1() {
+	/* 四暗刻 */
+	yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+		"四暗刻", yaku::yakuCalculator::Yaku::yval_yakuman_menzen,
+		[](const MENTSU_ANALYSIS* const analysis) -> bool {
+			return (analysis->TotalAnKezi - (
+				((analysis->Machi == yaku::yakuCalculator::machiShanpon) && (*analysis->TsumoAgariFlag)) ?
+				1 : 0) == 4);
+		}
+	));
+	/* 四暗刻単騎 */
+	if (getRule(RULE_DOUBLE_YAKUMAN) == 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"四暗刻単騎", yaku::yakuCalculator::Yaku::yval_double_yakuman_menzen,
+			"四暗刻",
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return (analysis->TotalAnKezi - (
+					((analysis->Machi == yaku::yakuCalculator::machiShanpon) && (*analysis->TsumoAgariFlag)) ?
+					1 : 0) == 4) && (
+					(analysis->GameStat->TianHuFlag) ||
+					((analysis->Machi == yaku::yakuCalculator::machiTanki) && (!analysis->MachiInfo.FuritenFlag))
+					);
+			}
+		));
+	/* 三暗刻 */
+	yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+		"三暗刻", yaku::yakuCalculator::Yaku::yval_2han,
+		[](const MENTSU_ANALYSIS* const analysis) -> bool {
+			return (analysis->TotalAnKezi - (
+				((analysis->Machi == yaku::yakuCalculator::machiShanpon) && (*analysis->TsumoAgariFlag)) ?
+				1 : 0) == 3);
+		}
+	));
 }
