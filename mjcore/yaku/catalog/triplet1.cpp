380fd3ba25fbc6e56b864c5107c1c0cb960db6a8
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-04 06:52:59 +0900

    三連刻とか四連刻とか

--- mjcore/yaku/catalog/triplet1.cpp
+++ mjcore/yaku/catalog/triplet1.cpp
@@ -33,4 +33,76 @@ void yaku::yakuCalculator::YakuCatalog::catalogInit::yakulst_triplet_1() {
 				1 : 0) == 3);
 		}
 	));
+
+	// ---------------------------------------------------------------------
+
+	/* 五連刻 */
+	if (getRule(RULE_GOLDEN_LADDER) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"金梯", yaku::yakuCalculator::Yaku::yval_double_yakuman,
+			"清一色", "対々和",
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				bool yakuFlag = false;
+				for (int i = 1; i < TILE_SUIT_HONORS; i++)
+					if ((analysis->KeziCount[i] >= 1) &&
+						(analysis->KeziCount[i + 1] >= 1) &&
+						(analysis->KeziCount[i + 2] >= 1) &&
+						(analysis->KeziCount[i + 3] >= 1) &&
+						((analysis->MianziDat[0].tile == (tileCode)(i - 1)) ||
+						(analysis->MianziDat[0].tile == (tileCode)(i + 4))) )
+						yakuFlag = true;
+				return yakuFlag;
+			}
+		));
+	/* 四連刻 */
+	auto suurenkoh =
+		[](const MENTSU_ANALYSIS* const analysis) -> bool {
+			bool yakuFlag = false;
+			for (int i = 1; i < TILE_SUIT_HONORS; i++)
+				if ((analysis->KeziCount[i] >= 1) &&
+					(analysis->KeziCount[i + 1] >= 1) &&
+					(analysis->KeziCount[i + 2] >= 1) &&
+					(analysis->KeziCount[i + 3] >= 1))
+					yakuFlag = true;
+			return yakuFlag;
+		};
+	if (getRule(RULE_SUURENKOH) == 1)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"四連刻", yaku::yakuCalculator::Yaku::yval_yakuman,
+			"金梯", "対々和",
+			suurenkoh
+		));
+	else if (getRule(RULE_SUURENKOH) == 2)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"四連刻", yaku::yakuCalculator::Yaku::yval_6han /* 対々和込み8飜 */,
+			"金梯",
+			suurenkoh
+		));
+	/* 三連刻 */
+	auto sanrenkoh =
+		[](const MENTSU_ANALYSIS* const analysis) -> bool {
+			bool yakuFlag = false;
+			for (int i = 1; i < TILE_SUIT_HONORS; i++)
+				if ((analysis->KeziCount[i] >= 1) &&
+					(analysis->KeziCount[i + 1] >= 1) &&
+					(analysis->KeziCount[i + 2] >= 1))
+					yakuFlag = true;
+			return yakuFlag;
+		};
+	if (getRule(RULE_SANRENKOH) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"三連刻", (getRule(RULE_SANRENKOH) == 2) ?
+			yaku::yakuCalculator::Yaku::yval_3han : yaku::yakuCalculator::Yaku::yval_2han,
+			"金梯", "四連刻",
+			sanrenkoh
+		));
+	/* 鏡音レン */
+	if (getRule(RULE_KAGAMINE_LEN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"鏡音レン", yaku::yakuCalculator::Yaku::yval_2han,
+			/* 三連刻に追加する役 */
+			[sanrenkoh](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return ((analysis->KeziCount[WhiteDragon] >= 1) && sanrenkoh(analysis));
+			}
+		));
 }
