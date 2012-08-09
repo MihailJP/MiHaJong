869fa94baaf81a82eb619e6f35135e5a4fe92111
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-10 01:07:37 +0900

    奇数対々和、偶数対々和

--- mjcore/yaku/catalog/triplet2.cpp
+++ mjcore/yaku/catalog/triplet2.cpp
@@ -1,4 +1,28 @@
 #include "../catalog.h"
 
 void yaku::yakuCalculator::YakuCatalog::catalogInit::yakulst_triplet_2() {
+	/* Šï”‘ÎX˜a */
+	if (getRule(RULE_ODD_TOITOI) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"Šï”‘ÎX˜a", yaku::yakuCalculator::Yaku::yval_4han,
+			"‘ÎX˜a",
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				int count = 0;
+				for (int i = 1; i < TILE_SUIT_HONORS; i += 2)
+					if (analysis->DuiziCount[i] >= 1) ++count;
+				return (count == SIZE_OF_MELD_BUFFER);
+			}
+		));
+	/* ‹ô”‘ÎX˜a */
+	if (getRule(RULE_EVEN_TOITOI) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"‹ô”‘ÎX˜a", yaku::yakuCalculator::Yaku::yval_5han,
+			"‘ÎX˜a", "’f›ô‹ã",
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				int count = 0;
+				for (int i = 2; i < TILE_SUIT_HONORS; i += 2)
+					if (analysis->DuiziCount[i] >= 1) ++count;
+				return (count == SIZE_OF_MELD_BUFFER);
+			}
+		));
 }
