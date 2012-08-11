3f5a2df07ef24026c96154b6465c533f884b1ee6
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-11 16:43:16 +0900

    幾つかの役を再実装(ファイル進捗率25%)

--- mjcore/yaku/catalog/misc.cpp
+++ mjcore/yaku/catalog/misc.cpp
@@ -26,36 +26,32 @@ void yaku::yakuCalculator::YakuCatalog::catalogInit::yakulst_misc() {
 				const tileCode shunzi[] = {BambooTwo};
 				return chktiles(analysis, kezi, 6, shunzi, 1, false);
 			};
-		auto withdragon =
-			[chktiles](const MENTSU_ANALYSIS* const analysis) -> bool {
-				const tileCode kezi[] = {GreenDragon};
-				if (analysis->shanten[shantenRegular] == -1)
-					return (yaku::countingFacility::countSpecMentz
-						(analysis->MianziDat, kezi, 1, NULL, 0, false) > 0);
-				else if (analysis->shanten[shantenPairs] == -1)
-					return (yaku::countingFacility::countPairs(analysis->TileCount, kezi, 1) > 0);
-				else return false;
-			};
-		if (getRule(RULE_DRAGONLESS_ALL_GREEN) == 0) // 發なしでも良いルール
+		if (getRule(RULE_DRAGONLESS_ALL_GREEN) != 0)
+			yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+				"緑一色", yaku::yakuCalculator::Yaku::yval_yakuman,
+				"混一色",
+				[allgrean](const MENTSU_ANALYSIS* const analysis) -> bool {
+					return allgrean(analysis) && (analysis->TileCount[GreenDragon] > 0);
+				}
+			));
+		else if (chkGameType(&GameStat, SanmaS))
+			yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+				"緑一色", yaku::yakuCalculator::Yaku::yval_yakuman,
+				"混一色", "清一色",
+				allgrean
+			));
+		else // 發なしでも良いルール
 			yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
 				"緑一色", yaku::yakuCalculator::Yaku::yval_yakuman,
 				"混一色", // 清一色は複合可
 				allgrean
 			));
-		else
-			yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
-				"緑一色", yaku::yakuCalculator::Yaku::yval_yakuman,
-				"混一色",
-				[allgrean, withdragon](const MENTSU_ANALYSIS* const analysis) -> bool {
-					return allgrean(analysis) && (withdragon(analysis));
-				}
-			));
 		if (getRule(RULE_DRAGONLESS_ALL_GREEN) == 2) // 發なしがダブル役満になるルール
 			yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
 				chkGameType(&GameStat, SanmaS) ? "清緑一色" : "緑一色", yaku::yakuCalculator::Yaku::yval_double_yakuman,
 				"清一色",
-				[allgrean, withdragon](const MENTSU_ANALYSIS* const analysis) -> bool {
-					return allgrean(analysis) && (!withdragon(analysis));
+				[allgrean](const MENTSU_ANALYSIS* const analysis) -> bool {
+					return allgrean(analysis) && (analysis->TileCount[GreenDragon] == 0);
 				}
 			));
 	}
@@ -106,4 +102,141 @@ void yaku::yakuCalculator::YakuCatalog::catalogInit::yakulst_misc() {
 				}
 			));
 	}
+
+	// ---------------------------------------------------------------------
+
+	/* 推不倒 */
+	if (getRule(RULE_TOIPUUTAO) != 0) {
+		yaku::yakuCalculator::Yaku::HANFUNC han;
+		switch (getRule(RULE_TOIPUUTAO)) {
+			case 1: han = yaku::yakuCalculator::Yaku::yval_1han; break;
+			case 2: han = yaku::yakuCalculator::Yaku::yval_4han; break;
+			case 3: han = yaku::yakuCalculator::Yaku::yval_yakuman; break;
+		}
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"推不倒", han,
+			"絶一門",
+			[chktiles](const MENTSU_ANALYSIS* const analysis) -> bool {
+				const tileCode kezi[] = {
+					CircleOne, CircleTwo, CircleThree, CircleFour, CircleFive, CircleEight, CircleNine,
+					BambooTwo, BambooFour, BambooFive, BambooSix, BambooEight, BambooNine, WhiteDragon,
+				};
+				const tileCode shunzi[] = {
+					CircleOne, CircleTwo, CircleThree, BambooFour,
+				};
+				return chktiles(analysis, kezi, 14, shunzi, 4, false);
+			}
+		));
+	}
+	/* 紅孔雀 */
+	if (getRule(RULE_BENIKUJAKU) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"紅孔雀", yaku::yakuCalculator::Yaku::yval_yakuman,
+			"混一色", "対々和",
+			[chktiles](const MENTSU_ANALYSIS* const analysis) -> bool {
+				const tileCode kezi[] = {
+					BambooOne, BambooFive, BambooSeven, BambooNine, RedDragon,
+				};
+				return chktiles(analysis, kezi, 5, NULL, 0, false);
+			}
+		));
+	/* 萬緑叢中一点紅 */
+	if (getRule(RULE_KOUITTEN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"紅一点", yaku::yakuCalculator::Yaku::yval_yakuman,
+			"混一色",
+			[chktiles](const MENTSU_ANALYSIS* const analysis) -> bool {
+				const tileCode kezi[] = {
+					BambooTwo, BambooThree, BambooFour, BambooSix, BambooEight, RedDragon,
+				};
+				const tileCode shunzi[] = {
+					BambooTwo,
+				};
+				return chktiles(analysis, kezi, 6, shunzi, 1, false) &&
+					(analysis->TileCount[RedDragon] > 0);
+			}
+		));
+	/* 白有り緑一色 */
+	if (getRule(RULE_ALL_GREEN_WITH_WHITE) != 0) {
+		auto allgreen_white =
+			[chktiles](const MENTSU_ANALYSIS* const analysis) -> bool {
+				const tileCode kezi[] = {
+					BambooTwo, BambooThree, BambooFour, BambooSix, BambooEight, WhiteDragon,
+				};
+				const tileCode shunzi[] = {
+					BambooTwo,
+				};
+				return chktiles(analysis, kezi, 6, shunzi, 1, false) &&
+					(analysis->TileCount[WhiteDragon] > 0);
+			};
+		if (getRule(RULE_ALL_GREEN_WITH_WHITE) == 1)
+			yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+				"清二色", yaku::yakuCalculator::Yaku::yval_2han,
+				allgreen_white
+			));
+		else if (getRule(RULE_ALL_GREEN_WITH_WHITE) == 2)
+			yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+				"白有り緑一色", yaku::yakuCalculator::Yaku::yval_yakuman,
+				"混一色",
+				allgreen_white
+			));
+	}
+	/* 緑一色輪 */
+	if (getRule(RULE_RYUUIISOHRIN) != 0) {
+		auto allgreen_five =
+			[chktiles](const MENTSU_ANALYSIS* const analysis) -> bool {
+				const tileCode kezi[] = {
+					BambooTwo, BambooThree, BambooFour, BambooFive, BambooSix, BambooEight,
+				};
+				const tileCode shunzi[] = {
+					BambooTwo,
+				};
+				return chktiles(analysis, kezi, 6, shunzi, 1, false) &&
+					(analysis->MianziDat[0].tile == BambooFive);
+			};
+		if (chkGameType(&GameStat, SanmaS))
+			yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+				"緑一色輪", yaku::yakuCalculator::Yaku::yval_yakuman,
+				"清一色",
+				allgreen_five
+			));
+		else
+			yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+				"緑一色輪", yaku::yakuCalculator::Yaku::yval_yakuman,
+				allgreen_five
+			));
+	}
+	/* 大草原 */
+	if (getRule(RULE_DAISOUGEN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"大草原", yaku::yakuCalculator::Yaku::yval_yakuman,
+			"清一色",
+			[chktiles](const MENTSU_ANALYSIS* const analysis) -> bool {
+				const tileCode kezi[] = {
+					BambooTwo, BambooThree, BambooFour, BambooFive,
+					BambooSix, BambooSeven, BambooNine,
+				};
+				const tileCode shunzi[] = {
+					BambooTwo, BambooThree, BambooFour, BambooFive,
+				};
+				return chktiles(analysis, kezi, 7, shunzi, 4, false);
+			}
+		));
+	/* 南大草原 */
+	if (getRule(RULE_SOUTH_DAISOUGEN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"南大草原", yaku::yakuCalculator::Yaku::yval_yakuman,
+			"混一色",
+			[chktiles](const MENTSU_ANALYSIS* const analysis) -> bool {
+				const tileCode kezi[] = {
+					BambooTwo, BambooThree, BambooFour, BambooFive,
+					BambooSix, BambooSeven, BambooNine, SouthWind,
+				};
+				const tileCode shunzi[] = {
+					BambooTwo, BambooThree, BambooFour, BambooFive,
+				};
+				return chktiles(analysis, kezi, 7, shunzi, 4, false) &&
+					(analysis->TileCount[SouthWind] > 0);
+			}
+		));
 }
