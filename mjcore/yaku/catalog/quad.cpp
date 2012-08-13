59ad0587c5209bb350e914d0ba05c91323df235c
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-13 23:54:56 +0900

    槓子役を追加(鬼は外の立春判定部分以外)

--- mjcore/yaku/catalog/quad.cpp
+++ mjcore/yaku/catalog/quad.cpp
@@ -1,4 +1,174 @@
 #include "../catalog.h"
 
 void yaku::yakuCalculator::YakuCatalog::catalogInit::yakulst_quad() {
+	/* 超四喜 */
+	if (getRule(RULE_CHOUSIXI) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"超四喜", yaku::yakuCalculator::Yaku::yval_triple_yakuman,
+			"大四喜", "四槓子", "対々和", "声東撃西", "走南闖北",
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return ((analysis->KangziCount[EastWind] >= 1) &&
+					(analysis->KangziCount[SouthWind] >= 1) &&
+					(analysis->KangziCount[WestWind] >= 1) &&
+					(analysis->KangziCount[NorthWind] >= 1));
+			}
+		));
+	/* 四槓子 */
+	yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+		"四槓子", ((getRule(RULE_SUUKANTSU) == 1) || (getRule(RULE_SUUKANTSU) == 3)) ?
+		yaku::yakuCalculator::Yaku::yval_double_yakuman : yaku::yakuCalculator::Yaku::yval_yakuman,
+		"対々和",
+		[](const MENTSU_ANALYSIS* const analysis) -> bool {
+			return analysis->TotalKangzi == 4;
+		}
+	));
+	/* 三暗槓 */
+	if (getRule(RULE_SANANKAN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"三暗槓", yaku::yakuCalculator::Yaku::yval_yakuman,
+			"三槓子",
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return analysis->TotalAnKangzi == 3;
+			}
+		));
+	/* 大峡谷 */
+	if (getRule(RULE_GRAND_CANYON) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"大峡谷", yaku::yakuCalculator::Yaku::yval_yakuman,
+			"三槓子",
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return ((analysis->KangziCount[CharacterNine] >= 1) &&
+					(analysis->KangziCount[CircleNine] >= 1) &&
+					(analysis->KangziCount[BambooNine] >= 1) &&
+					(analysis->DuiziCount[CharacterEight] + analysis->DuiziCount[CircleEight] +
+					analysis->DuiziCount[BambooEight] >= 2));
+			}
+		));
+	/* 雪化粧 */
+	if (getRule(RULE_YUKIGESHOU) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"雪化粧", yaku::yakuCalculator::Yaku::yval_yakuman,
+			"役牌・白", "三槓子",
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return ((analysis->KangziCount[CircleOne] >= 1) &&
+					(analysis->KangziCount[CircleNine] >= 1) &&
+					(analysis->KangziCount[WhiteDragon] >= 1));
+			}
+		));
+	/* 三色同槓 */
+	if (getRule(RULE_SANSHOKU_DOUKAN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"三色同槓", (getRule(RULE_SANSHOKU_DOUKAN) == 2) ?
+			(yaku::yakuCalculator::Yaku::HANFUNC)yaku::yakuCalculator::Yaku::yval_yakuman :
+			(yaku::yakuCalculator::Yaku::HANFUNC)yaku::yakuCalculator::Yaku::yval_5han_kuisagari,
+			"三色同刻", "三槓子",
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				for (int i = 1; i < 9; i++)
+					if ((analysis->KangziCount[TILE_SUIT_CHARACTERS + i] >= 1) &&
+						(analysis->KangziCount[TILE_SUIT_CIRCLES + i] >= 1) &&
+						(analysis->KangziCount[TILE_SUIT_BAMBOOS + i] >= 1))
+						return true;
+				return false;
+			}
+		));
+	/* 超三元 */
+	if (getRule(RULE_CHOUSANGEN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"超三元", yaku::yakuCalculator::Yaku::yval_double_yakuman,
+			"大三元", "三槓子",
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return ((analysis->KangziCount[WhiteDragon] >= 1) &&
+					(analysis->KangziCount[GreenDragon] >= 1) &&
+					(analysis->KangziCount[RedDragon] >= 1));
+			}
+		));
+	/* 三槓子 */
+	yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+		"三槓子", (getRule(RULE_SANKANTSU) == 2) ? yaku::yakuCalculator::Yaku::yval_yakuman :
+		((getRule(RULE_SANKANTSU) == 1) ? yaku::yakuCalculator::Yaku::yval_3han :
+		yaku::yakuCalculator::Yaku::yval_2han),
+		[](const MENTSU_ANALYSIS* const analysis) -> bool {
+			return analysis->TotalKangzi == 3;
+		}
+	));
+	/* 二暗槓 */
+	if (getRule(RULE_RYANANKAN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"二暗槓", yaku::yakuCalculator::Yaku::yval_1han,
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return analysis->TotalAnKangzi == 2;
+			}
+		));
+	/* 声東撃西 */
+	if (getRule(RULE_SHENGDONG_JIXI) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"声東撃西", yaku::yakuCalculator::Yaku::yval_6han,
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return ((analysis->KangziCount[EastWind] >= 1) &&
+					(analysis->KangziCount[WestWind] >= 1));
+			}
+		));
+	/* 走南闖北 */
+	if (getRule(RULE_ZAONAN_CHUANGBEI) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"走南闖北", yaku::yakuCalculator::Yaku::yval_6han,
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return ((analysis->KangziCount[SouthWind] >= 1) &&
+					(analysis->KangziCount[NorthWind] >= 1));
+			}
+		));
+	/* 暗中模索 */
+	if (getRule(RULE_ANCHUMOSAKU) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"暗中模索", yaku::yakuCalculator::Yaku::yval_1han,
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return ((analysis->AnKangziCount[RedDragon] >= 1) &&
+					(analysis->TsumoHai->tile / TILE_SUIT_STEP == TILE_SUIT_BAMBOOS / TILE_SUIT_STEP) &&
+					(*analysis->TsumoAgariFlag));
+			}
+		));
+	/* 戦車 */
+	if (getRule(RULE_TANK) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"戦車", yaku::yakuCalculator::Yaku::yval_1han,
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return (analysis->KangziCount[BambooSeven] >= 1);
+			}
+		));
+	/* 真田六文銭 */
+	if (getRule(RULE_SANADACOIN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"真田六文銭", yaku::yakuCalculator::Yaku::yval_1han,
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return (analysis->KangziCount[CircleSix] >= 1);
+			}
+		));
+	/* 三矢の誓い */
+	if (getRule(RULE_THREE_ARROWS) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"三矢の誓い", yaku::yakuCalculator::Yaku::yval_1han,
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				return (analysis->KangziCount[BambooThree] >= 1);
+			}
+		));
+	/* 鬼は外 */
+	if (getRule(RULE_SETSUBUN) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"鬼は外", yaku::yakuCalculator::Yaku::yval_1han,
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				/* TODO: 翌日が立春か判定する */
+				return (analysis->KangziCount[CircleNine] >= 1);
+			}
+		));
+	/* 草加 */
+	if (getRule(RULE_SOUKA) != 0)
+		yaku::yakuCalculator::YakuCatalog::Instantiate()->catalog.push_back(Yaku(
+			"草加", yaku::yakuCalculator::Yaku::yval_1han,
+			[](const MENTSU_ANALYSIS* const analysis) -> bool {
+				for (int i = 1; i < 9; i++)
+					if (analysis->KaKangziCount[TILE_SUIT_BAMBOOS + i] >= 1)
+						return true;
+				return false;
+			}
+		));
 }
