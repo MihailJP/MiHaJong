9751e03b79a4a36172e180a04c44a6a8a9b696b5
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-14 17:17:55 +0900

    バージョン情報修正

--- mjcore/version.h
+++ mjcore/version.h
@@ -2,14 +2,14 @@
 #define VERSION_H
 
 #define MIHAJONG_MAJOR_VER 1
-#define MIHAJONG_MINOR_VER 6
-#define MIHAJONG_PATCH_VER 5
-#define MIHAJONG_VER "1.6.5"
+#define MIHAJONG_MINOR_VER 7
+#define MIHAJONG_PATCH_VER 0
+#define MIHAJONG_VER "1.7.0"
 
 #define MJCORE_MAJOR_VER 1
-#define MJCORE_MINOR_VER 6
-#define MJCORE_PATCH_VER 5
-#define MJCORE_VER "1.6.5"
+#define MJCORE_MINOR_VER 7
+#define MJCORE_PATCH_VER 0
+#define MJCORE_VER "1.7.0"
 
 #define LOGGER_MAJOR_VER 1
 #define LOGGER_MINOR_VER 6
@@ -17,8 +17,8 @@
 #define LOGGER_VER "1.6.0"
 
 #define ASTRO_MAJOR_VER 1
-#define ASTRO_MINOR_VER 6
-#define ASTRO_PATCH_VER 1
-#define ASTRO_VER "1.6.1"
+#define ASTRO_MINOR_VER 7
+#define ASTRO_PATCH_VER 0
+#define ASTRO_VER "1.7.0"
 
 #endif
