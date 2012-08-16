3946bd52c03d3d7e6a4d1a1edfeb26d67942d884
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-17 01:33:43 +0900

    CSVファイル移動

--- mjcore/resource.h
+++ mjcore/resource.h
@@ -3,4 +3,6 @@
 #endif
 
 #define LZMA_STREAM                             256
+#define CSV_TABLE                               257
 #define IDR_LZMA_STREA1                         100
+#define IDR_CSV_TABL1                           101
