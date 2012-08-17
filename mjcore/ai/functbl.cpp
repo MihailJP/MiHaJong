05d813840f0ce0d7ead69b05cd991cf785380f20
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-17 20:08:31 +0900

    mihajong.Call実装

--- mjcore/ai/functbl.cpp
+++ mjcore/ai/functbl.cpp
@@ -4,7 +4,8 @@ const char* aiscript::table::functable::tblname = "mihajong"; // table name
 
 void aiscript::table::functable::inittable(lua_State* const L) {
 	lua_newtable(L); // table 'mihajong'
-	discardTileCode(L);
+	discardTileCode(L); // subtable 'DiscardType'
+	meldCallCode(L); // subtable 'Call'
 	lua_setglobal(L, tblname); // global table
 }
 inline void aiscript::table::functable::discardTileCode(lua_State* const L) {
@@ -17,5 +18,18 @@ inline void aiscript::table::functable::discardTileCode(lua_State* const L) {
 	TableAdd(L, "OpenRiichi", (int)DiscardTileNum::OpenRiichi);
 	TableAdd(L, "Agari", (int)DiscardTileNum::Agari);
 	TableAdd(L, "Kyuushu", (int)DiscardTileNum::Kyuushu);
+	lua_setfield(L, -2, "DiscardType");
+}
+inline void aiscript::table::functable::meldCallCode(lua_State* const L) {
+	lua_newtable(L);
+	TableAdd(L, "None", meldNone);
+	TableAdd(L, "Ron", meldRon);
+	TableAdd(L, "Kan", meldKan);
+	TableAdd(L, "Pon", meldPon);
+	lua_newtable(L);
+	TableAdd(L, "Lower", meldChiiLower);
+	TableAdd(L, "Middle", meldChiiMiddle);
+	TableAdd(L, "Upper", meldChiiUpper);
+	lua_setfield(L, -2, "Chii");
 	lua_setfield(L, -2, "DiscardType");
 }
--- mjcore/ai/functbl.h
+++ mjcore/ai/functbl.h
@@ -4,10 +4,15 @@
 #include <lua.hpp>
 #include "class.h"
 
+enum MeldCallID {
+	meldNone, meldRon, meldKan, meldPon, meldChiiLower, meldChiiMiddle, meldChiiUpper
+};
+
 class aiscript::table::functable {
 private:
 	static const char* tblname;
 	static inline void discardTileCode(lua_State* const L);
+	static inline void meldCallCode(lua_State* const L);
 public:
 	static void inittable(lua_State* const L);
 };
