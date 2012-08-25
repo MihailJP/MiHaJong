34b8a3d00768b7aba8ee30b36b97fd0a06949c97
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-26 06:17:18 +0900

    名状しがたいCSVから名状しがたい人間可読ドキュメントのようなものに変換する名状しがたいRubyスクリプトのようなもの

---
+++ auxtools/rulesyms.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# -*- coding: shift_jis -*-
+
+def gametype (type)
+	typecode = type.to_i
+	if typecode & 0x0f == 0x0f then return "" end
+	tmparr = []
+	if typecode & 0x01 != 0 then tmparr += ["Žl–ƒ"] end
+	if typecode & 0x02 != 0 then tmparr += ["ŽO–ƒ"] end
+	if typecode & 0x04 != 0 then tmparr += ["ŽllŽO–ƒ"] end
+	if typecode & 0x08 != 0 then tmparr += ["””vŽO–ƒ"] end
+	return "q" + tmparr.join(", ") + "r"
+end
+
+require "csv"
+csvdat = CSV.read("../mjcore/data/confitem.csv", encoding: "SJIS")
+
+for row in csvdat
+	print "y", row[8], "z", row[9], gametype(row[1]), "\n"
+	print "\t", row[10], "\n"
+	print "\tÝ’è’l: ", row[11..-1].keep_if{|s| s}.delete_if{|s| s == ">>>"}.join(", "), "\n\n"
+end
