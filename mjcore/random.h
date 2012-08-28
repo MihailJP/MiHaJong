9273dc39aa12be4cb1b231c5448ac6d47a3b9124
TANIGAWA Takashi <mihailjp@gmail.com>
2012-08-29 02:08:51 +0900

    中身実装

--- mjcore/random.h
+++ mjcore/random.h
@@ -1,5 +1,22 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <cstdint>
+
+class RndNum {
+private:
+	static std::seed_seq seed;
+	static std::mt19937 engine;
+public:
+	__declspec(dllexport) static void init();
+	__declspec(dllexport) static unsigned int rnd(unsigned int);
+	static double rnd();
+	__declspec(dllexport) static void frnd(double* const);
+	__declspec(dllexport) static unsigned int dice();
+};
 
 #endif
