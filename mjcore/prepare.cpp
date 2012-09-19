c6228a0e88971fa0d5175009f170aee6aec0d6a1
TANIGAWA Takashi <mihailjp@gmail.com>
2012-09-20 00:09:06 +0900

    赤ドラの初期化処理を移植

--- mjcore/prepare.cpp
+++ mjcore/prepare.cpp
@@ -3,7 +3,7 @@
 #define settile(tilecode, pos) {tilepos[tilecode] = pos; for (unsigned int i = 0; i < 4u; ++i) {gameStat->Deck[pos++].tile = tilecode;}}
 __declspec(dllexport) void shuffle(GameTable* const gameStat) { // îvÇÉoÉbÉtÉ@Ç…ï¿Ç◊ÇƒÅAêÙîv
 	UInt8ByTile tilepos;
-	{
+	{ // îvÇï¿Ç◊ÇÈ
 		unsigned int p = 0; // îvÇÃà íuID
 		if (chkGameType(gameStat, SanmaX)) {
 			settile(CharacterOne, p); // ‰›éq
@@ -27,6 +27,87 @@ __declspec(dllexport) void shuffle(GameTable* const gameStat) { // îvÇÉoÉbÉtÉ@Ç
 				gameStat->Deck[p++].tile = Plum; gameStat->Deck[p++].tile = Orchid;
 				gameStat->Deck[p++].tile = Chrysanthemum; gameStat->Deck[p++].tile = Bamboo;
 			}
+		}
+	}
+
+	{ // ê‘ÉhÉâÇê›íËÇ∑ÇÈ
+		const char tileRules[9][16] = {
+			"red_one", "red_two", "red_three", "red_four", "red_five",
+			"red_six", "red_seven", "red_eight", "red_nine",
+		};
+		for (unsigned int i = 0; i < 9; ++i) { // êîîvÇÃê‘ÉhÉâ
+			if (RuleData::chkRule(tileRules[i], "character_suit") || // ‰›éq
+				RuleData::chkRule(tileRules[i], "character_circle_suit") ||
+				RuleData::chkRule(tileRules[i], "character_bamboo_suit") ||
+				RuleData::chkRule(tileRules[i], "all_three_suits"))
+				gameStat->Deck[tilepos[TILE_SUIT_CHARACTERS + i + 1]].red = AkaDora;
+			if (RuleData::chkRule(tileRules[i], "circle_suit") || // ìõéq
+				RuleData::chkRule(tileRules[i], "character_circle_suit") ||
+				RuleData::chkRule(tileRules[i], "circle_bamboo_suit") ||
+				RuleData::chkRule(tileRules[i], "all_three_suits"))
+				gameStat->Deck[tilepos[TILE_SUIT_CIRCLES + i + 1]].red = AkaDora;
+			if (RuleData::chkRule(tileRules[i], "bamboo_suit") || // çıéq
+				RuleData::chkRule(tileRules[i], "circle_bamboo_suit") ||
+				RuleData::chkRule(tileRules[i], "character_bamboo_suit") ||
+				RuleData::chkRule(tileRules[i], "all_three_suits"))
+				gameStat->Deck[tilepos[TILE_SUIT_BAMBOOS + i + 1]].red = AkaDora;
+		}
+	}
+	// 5ÇÃÉhÉâÇÕì¡éÍ
+	if (chkGameType(gameStat, SanmaX)) { // éOñÉ
+		if (RuleData::chkRule("red_five", "2tiles")) {
+			gameStat->Deck[tilepos[CircleFive]].red = AkaDora;
+			gameStat->Deck[tilepos[BambooFive]].red = AkaDora;
+		}
+		else if (RuleData::chkRule("red_five", "4tiles")) {
+			gameStat->Deck[tilepos[CircleFive]].red = AkaDora;
+			gameStat->Deck[tilepos[CircleFive] + 1].red = AkaDora;
+			gameStat->Deck[tilepos[BambooFive]].red = AkaDora;
+			gameStat->Deck[tilepos[BambooFive] + 1].red = AkaDora;
+		}
+		else if (RuleData::chkRule("red_five", "2_circle_tiles")) {
+			gameStat->Deck[tilepos[CircleFive]].red = AkaDora;
+			gameStat->Deck[tilepos[CircleFive] + 1].red = AkaDora;
+		}
+	} else { // élñÉÅEêîîvéOñÉ
+		if (RuleData::chkRule("red_five", "3tiles")) {
+			gameStat->Deck[tilepos[CharacterFive]].red = AkaDora;
+			gameStat->Deck[tilepos[CircleFive]].red = AkaDora;
+			gameStat->Deck[tilepos[BambooFive]].red = AkaDora;
+		}
+		else if (RuleData::chkRule("red_five", "4tiles")) {
+			gameStat->Deck[tilepos[CharacterFive]].red = AkaDora;
+			gameStat->Deck[tilepos[CircleFive]].red = AkaDora;
+			gameStat->Deck[tilepos[CircleFive] + 1].red = AkaDora;
+			gameStat->Deck[tilepos[BambooFive]].red = AkaDora;
+		}
+		else if (RuleData::chkRule("red_five", "6tiles")) {
+			gameStat->Deck[tilepos[CharacterFive]].red = AkaDora;
+			gameStat->Deck[tilepos[CharacterFive] + 1].red = AkaDora;
+			gameStat->Deck[tilepos[CircleFive]].red = AkaDora;
+			gameStat->Deck[tilepos[CircleFive] + 1].red = AkaDora;
+			gameStat->Deck[tilepos[BambooFive]].red = AkaDora;
+			gameStat->Deck[tilepos[BambooFive] + 1].red = AkaDora;
+		}
+		else if (RuleData::chkRule("red_five", "2_circle_tiles")) {
+			gameStat->Deck[tilepos[CircleFive]].red = AkaDora;
+			gameStat->Deck[tilepos[CircleFive] + 1].red = AkaDora;
+		}
+	}
+	{
+		const char tileRules[2][16] = {"red_west", "red_north",};
+		const tileCode tileCodes[2] = {WestWind, NorthWind,};
+		for (unsigned int i = 0; i < 2; ++i) { // êîîvÇÃê‘ÉhÉâ
+			if (RuleData::chkRule(tileRules[i], "4tiles") || RuleData::chkRule(tileRules[i], "3tiles") ||
+				RuleData::chkRule(tileRules[i], "2tiles") || RuleData::chkRule(tileRules[i], "1tile"))
+				gameStat->Deck[tilepos[TILE_SUIT_CHARACTERS + i]].red = AkaDora;
+			if (RuleData::chkRule(tileRules[i], "4tiles") || RuleData::chkRule(tileRules[i], "3tiles") ||
+				RuleData::chkRule(tileRules[i], "2tiles"))
+				gameStat->Deck[tilepos[TILE_SUIT_CHARACTERS + i + 1]].red = AkaDora;
+			if (RuleData::chkRule(tileRules[i], "4tiles") || RuleData::chkRule(tileRules[i], "3tiles"))
+				gameStat->Deck[tilepos[TILE_SUIT_CHARACTERS + i + 2]].red = AkaDora;
+			if (RuleData::chkRule(tileRules[i], "4tiles"))
+				gameStat->Deck[tilepos[TILE_SUIT_CHARACTERS + i + 3]].red = AkaDora;
 		}
 	}
 }
