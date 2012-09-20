96acd60a6c9542c92337b0a961f740b47ce5b500
TANIGAWA Takashi <mihailjp@gmail.com>
2012-09-20 13:29:09 +0900

    ソケットDLLの初期化とクリンナップの処理

--- socket/socket.cpp
+++ socket/socket.cpp
@@ -1,1 +1,37 @@
 #include "socket.h"
+
+WSADATA SocketInfo;
+HINSTANCE dllInst;
+
+DLL int socket_init () { // ソケットを初期化する
+	try {
+		if (int err = WSAStartup(MAKEWORD(2, 0), &SocketInfo)) throw err;
+		return 0;
+	}
+	catch (int err) {
+		// TODO: ダイアログとか出す
+		return err;
+	}
+}
+
+DLL int socket_bye () { // ソケットのクリンナップ
+	return WSACleanup();
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) { // 初期化などを行う
+	switch (fdwReason) {
+	case DLL_PROCESS_ATTACH:
+		dllInst = hinstDLL;
+		socket_init();
+		break;
+	case DLL_PROCESS_DETACH:
+		dllInst = NULL;
+		socket_bye();
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+		break;
+	}
+
+	return TRUE;
+}
--- socket/socket.h
+++ socket/socket.h
@@ -1,4 +1,19 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 
+#include <WinSock2.h>
+
+#define NUM_OF_SOCKETS 32
+
+#ifdef SOCKET_EXPORTS
+#define DLL __declspec(dllexport)
+extern WSADATA SocketInfo;
+extern HINSTANCE dllInst;
+#else
+#define DLL __declspec(dllimport)
+#endif
+
+DLL int socket_init (); // ソケットを初期化する
+DLL int socket_bye (); // ソケットのクリンナップ
+
 #endif
