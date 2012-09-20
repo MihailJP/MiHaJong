9e7f1ab31bce1b257298256d1649d3d9fdafb2a9
TANIGAWA Takashi <mihailjp@gmail.com>
2012-09-20 19:20:53 +0900

    ワーカースレッドを作る、ほか幾つか修正

--- socket/class.cpp
+++ socket/class.cpp
@@ -11,19 +11,98 @@ uint32_t mihajong_socket::Sock::addr2var(const std::string& address) { // アドレ
 }
 
 mihajong_socket::Sock::Sock (const std::string& destination, uint16_t port) { // クライアント接続
-	sock = socket(AF_INET, SOCK_STREAM, 0);
+	sock = socket(AF_INET, SOCK_STREAM, 0); // ソケットを初期化
 	if (sock == INVALID_SOCKET) throw socket_creation_error(WSAGetLastError()); // ソケットの作成に失敗したら例外
 
+	this->connect(destination, port); // 接続
+}
+
+mihajong_socket::Sock::~Sock() { // 接続を切る
+	this->disconnect();
+}
+
+void mihajong_socket::Sock::connect (const std::string& destination, uint16_t port) { // クライアント接続
 	isServer = false; // クライアントである
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(port); // ポート番号
 	addr.sin_addr.S_un.S_addr = addr2var(destination.c_str()); // アドレス
-
-	if (::connect(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) // 接続
-		throw connection_failure(WSAGetLastError());
+	this->connect();
 }
 
-mihajong_socket::Sock::~Sock() { // 接続を切る
-	shutdown(sock, SD_BOTH);
+void mihajong_socket::Sock::connect () { // クライアント再接続
+	threadPtr.client = new client_thread();
+	threadPtr.client->setaddr(addr);
+	CreateThread(NULL, 0, client_thread::thread, (LPVOID)threadPtr.client, 0, NULL);
+}
+
+bool mihajong_socket::Sock::connected () { // 接続されているかを確認
+	if (isServer) {
+		return false; // 仮置き
+	} else {
+		threadPtr.client->chkError();
+		return threadPtr.client->isConnected();
+	}
+};
+
+void mihajong_socket::Sock::disconnect () { // 接続を切る
+	if (isServer) {
+		// 仮置き
+	} else {
+		threadPtr.client->terminate();
+	}
 	closesocket(sock);
+	if (isServer) {
+		// 仮置き
+	} else {
+		delete threadPtr.client;
+		threadPtr.client = NULL;
+	}
+}
+
+// -------------------------------------------------------------------------
+
+mihajong_socket::Sock::client_thread::client_thread() {
+	errtype = errNone; errcode = 0;
+	connected = terminated = finished = false;
+}
+
+DWORD WINAPI mihajong_socket::Sock::client_thread::thread(LPVOID lp) { // スレッドを起動するための処理
+	return ((client_thread*)lp)->myThreadFunc();
+}
+
+void mihajong_socket::Sock::client_thread::chkError () { // エラーをチェックし、もしエラーだったら例外を投げる
+	switch (errtype) {
+	case errNone: // エラーなし
+		break;
+	case errConnection: // 接続失敗
+		throw connection_failure(errcode);
+	default: // ほかのエラー
+		throw socket_error(errcode);
+	}
+}
+
+DWORD WINAPI mihajong_socket::Sock::client_thread::myThreadFunc() { // スレッドの処理
+	if (::connect(*mySock, (sockaddr*)&myAddr, sizeof(myAddr)) == SOCKET_ERROR) { // 接続
+		errtype = errConnection; errcode = WSAGetLastError(); ExitThread(-((int)errtype));
+	}
+	// TODO: ここにループを書く
+	return S_OK;
+}
+
+bool mihajong_socket::Sock::client_thread::isConnected() { // 接続済かを返す関数
+	return connected;
+}
+
+void mihajong_socket::Sock::client_thread::setaddr (const sockaddr_in destination) { // 接続先を設定する
+	myAddr = destination;
+}
+void mihajong_socket::Sock::client_thread::setsock (SOCKET* const socket) { // ソケットを設定する
+	mySock = socket;
+}
+
+void mihajong_socket::Sock::client_thread::terminate () { // 切断する
+	terminated = true; // フラグを立てる
+	while (!finished) Sleep(0); // スレッドが終了するまで待つ
+	finished = terminated = connected = false; // フラグの後始末
+	errtype = errNone; errcode = 0;
 }
--- socket/class.h
+++ socket/class.h
@@ -10,13 +10,51 @@ namespace mihajong_socket {
 
 class Sock {
 private:
+	class client_thread; // クライアントのスレッド
+	class server_thread; // サーバーのスレッド
+	union Thread { // スレッドオブジェクトのポインタ(共用体)
+	public:
+		client_thread* client;
+		server_thread* server;
+	};
 	static uint32_t addr2var(const std::string& address); // アドレスを取得
 	bool isServer;
 	sockaddr_in addr;
 	SOCKET sock;
+	Thread threadPtr;
 public:
+	Sock () {}; // ソケット初期化
 	Sock (const std::string& destination, uint16_t port); // クライアント接続
 	~Sock (); // 接続を切る
+	void connect (const std::string& destination, uint16_t port); // クライアント接続
+	void connect (); // クライアント再接続
+	bool connected (); // 接続されているかを確認
+	void disconnect (); // 接続を切る
+};
+
+class Sock::client_thread { // クライアントのスレッド
+public:
+	enum errorType {errNone, errConnection,};
+	client_thread();
+	static DWORD WINAPI thread(LPVOID lp); // スレッドを起動するための処理
+	bool isConnected (); // 接続済かを返す関数
+	void setaddr (const sockaddr_in destination); // 接続先を設定する
+	void setsock (SOCKET* const socket); // ソケットを設定する
+	void terminate (); // 切断する
+	void chkError (); // エラーをチェックし、もしエラーだったら例外を投げる
+private:
+	SOCKET* mySock; // ソケット(ポインタ)
+	errorType errtype; // エラーの種類
+	int errcode; // エラーコード
+	bool connected; // 接続済みかのフラグ[ワーカースレッドから書き込み]
+	bool terminated; // 接続済みかのフラグ[親スレッドから書き込み]
+	bool finished; // 終了済みかのフラグ[ワーカースレッドから書き込み]
+	sockaddr_in myAddr; // アドレス情報[親スレッドから書き込み]
+	DWORD WINAPI myThreadFunc(); // スレッドの処理
+};
+
+class Sock::server_thread { // サーバーのスレッド
+	// TODO: これを実装する
 };
 
 }
