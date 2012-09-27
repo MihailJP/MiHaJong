6bc2d6eac47d7ada5f145d3b1189ea6d386288ca
TANIGAWA Takashi <mihailjp@gmail.com>
2012-09-27 21:23:06 +0900

    clientsend移植

--- socket/client.cpp
+++ socket/client.cpp
@@ -3,7 +3,13 @@
 namespace mihajong_socket {
 namespace client {
 
-
+	void send (unsigned char SendingMsg) { // サーバーにメッセージを送る
+		sockets[0]->putc(SendingMsg);
+		// かつてはここでログ出力していた
+	}
+	DLL void send (int SendingMsg) { // サーバーにメッセージを送る [Transitional API]
+		send((unsigned char)SendingMsg);
+	}
 
 }
 }
--- socket/client.h
+++ socket/client.h
@@ -1,13 +1,15 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include "socket.h"
 #include "server.h"
 
 namespace mihajong_socket {
 namespace client {
 	using server::getString; using server::putString;
 
-
+	void send (unsigned char SendingMsg); // サーバーにメッセージを送る
+	DLL void send (int SendingMsg); // サーバーにメッセージを送る [Transitional API]
 }
 }
 
