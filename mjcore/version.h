a627178a93be044502e0d5041c7750946a30fe71
TANIGAWA Takashi <mihailjp@gmail.com>
2012-10-22 22:49:54 +0900

    バージョン番号を設定(1.8.0-β1)

--- mjcore/version.h
+++ mjcore/version.h
@@ -2,14 +2,14 @@
 #define VERSION_H
 
 #define MIHAJONG_MAJOR_VER 1
-#define MIHAJONG_MINOR_VER 7
-#define MIHAJONG_PATCH_VER 6
-#define MIHAJONG_VER "1.7.6"
+#define MIHAJONG_MINOR_VER 8
+#define MIHAJONG_PATCH_VER 0
+#define MIHAJONG_VER "1.8.0-beta1"
 
 #define MJCORE_MAJOR_VER 1
-#define MJCORE_MINOR_VER 7
-#define MJCORE_PATCH_VER 6
-#define MJCORE_VER "1.7.6"
+#define MJCORE_MINOR_VER 8
+#define MJCORE_PATCH_VER 0
+#define MJCORE_VER "1.8.0-beta1"
 
 #define LOGGER_MAJOR_VER 1
 #define LOGGER_MINOR_VER 6
@@ -24,6 +24,6 @@
 #define SOCKET_MAJOR_VER 1
 #define SOCKET_MINOR_VER 8
 #define SOCKET_PATCH_VER 0
-#define SOCKET_VER "1.8.0"
+#define SOCKET_VER "1.8.0-beta1"
 
 #endif
