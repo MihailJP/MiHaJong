3236b48bcb1ad6ae8e3077fdb66f3ecab266c167
TANIGAWA Takashi <mihailjp@gmail.com>
2012-11-16 14:14:19 +0900

    テキストファイルが抜けてるのに対処

--- package.bat
+++ package.bat
@@ -3,12 +3,14 @@ rmdir /s /q release
 mkdir release
 mkdir release\mihajong
 copy mihajong\*.exe release\mihajong
-del /q release\mihajong\logger.exe
 copy mihajong\*.dll release\mihajong
+del /q release\mihajong\logger.dll
 copy mihajong\readme.txt release\mihajong
 copy mihajong\license.txt release\mihajong
 copy mihajong\history.txt release\mihajong
 copy mihajong\playing.txt release\mihajong
+copy mihajong\script.txt release\mihajong
+copy mihajong\rulesyms.txt release\mihajong
 mkdir release\mihajong\bgm
 mkdir release\mihajong\ai
 copy mihajong\ai\*.* release\mihajong\ai
