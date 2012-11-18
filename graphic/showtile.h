41c1d9031e11dd4b78a6c247891915df9c9e1993
TANIGAWA Takashi <mihailjp@gmail.com>
2012-11-18 15:02:08 +0900

    牌の画像のリサイズを取り消し
    This reverts commit 6815f1093de735eeb359876866b8e53f80883aac.
    
    Conflicts:
    
    	graphic/scenes/table.cpp

--- graphic/showtile.h
+++ graphic/showtile.h
@@ -13,14 +13,14 @@ enum TileDirection {Portrait, Withershins, UpsideDown, Clockwise,};
 
 class ShowTile {
 public:
-	static const unsigned int TextureWidth   = 840;
-	static const unsigned int TextureHeight  = 1079;
-	static const unsigned int VertTileWidth  = 28;
-	static const unsigned int VertTileHeight = 47;
-	static const unsigned int SideTileWidth  = 18;
+	static const unsigned int TextureWidth   = 960;
+	static const unsigned int TextureHeight  = 1235;
+	static const unsigned int VertTileWidth  = 32;
+	static const unsigned int VertTileHeight = 54;
+	static const unsigned int SideTileWidth  = 20;
 	static const unsigned int SideTileHeight = VertTileHeight;
-	static const unsigned int HoriTileWidth  = 40;
-	static const unsigned int HoriTileHeight = 36;
+	static const unsigned int HoriTileWidth  = 46;
+	static const unsigned int HoriTileHeight = 41;
 	static const unsigned int TileCols = 10;
 	static const unsigned int TileRows = 13;
 private:
