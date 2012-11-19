08f64f98fe04dc770f44dd3875ee8a67de2b4e8a
TANIGAWA Takashi <mihailjp@gmail.com>
2012-11-19 23:02:16 +0900

    フォント画像を用意
    使用したフォントは「梅ゴシックO5」
    
    ライセンスは次の通り：
    これらのフォントはフリー（自由な）ソフトウエアです。あらゆる改変の有無に関わらず、また商業的な利用であっても、自由にご利用、複製、再配布することができますが、全て無保証とさせていただきます。

---
+++ graphic/text.cpp
@@ -0,0 +1,57 @@
+#include "text.h"
+#include "resource.h"
+#include "font.h"
+#include "../mjcore/strcode.h"
+
+TextRenderer::TextRenderer(LPDIRECT3DDEVICE9 device) {
+	myDevice = device;
+	LoadTexture(device, &font, MAKEINTRESOURCE(IDB_PNG_FONT), 1280, 1280);
+}
+
+TextRenderer::~TextRenderer() {
+	for (auto k = StringData.begin(); k != StringData.end(); ++k) {
+		if (*k) {
+			delete (*k); (*k) = nullptr;
+		}
+	}
+	if (font) font->Release();
+}
+
+/* 新規の文字列オブジェクトを作成する */
+void TextRenderer::NewText(unsigned int ID, const std::wstring& str, int x, int y, float scale = 1.0, float width = 1.0) {
+	if (StringData.size() <= ID) StringData.resize(ID + 1, nullptr); // 配列の拡張
+	if (!StringData[ID]) delete StringData[ID]; // 既に存在した場合
+	StringData[ID] = new StringAttr;
+	StringData[ID]->str = std::wstring(str);
+	StringData[ID]->X = x; StringData[ID]->Y = y;
+	StringData[ID]->scale = scale; StringData[ID]->width = width;
+	reconstruct();
+}
+void TextRenderer::NewText(unsigned int ID, const std::string& str, int x, int y, float scale = 1.0, float width = 1.0) {
+	NewText(ID, CodeConv::ANSItoWIDE(str), x, y, scale, width);
+}
+
+/* 文字列オブジェクトの後始末 */
+void TextRenderer::DelText(unsigned int ID) {
+	deleteSprite();
+	delete StringData[ID]; StringData[ID] = nullptr;
+	reconstruct();
+}
+
+/* スプライト再構築 */
+void TextRenderer::reconstruct() {
+	deleteSprite();
+	for (auto k = StringData.begin(); k != StringData.end(); ++k) {
+		
+	}
+}
+
+/* スプライトを削除する */
+void TextRenderer::deleteSprite() {
+	for (auto k = SpriteData.begin(); k != SpriteData.end(); ++k) {
+		if ((*k)->sprite) {
+			(*k)->sprite->Release(); (*k)->sprite = nullptr;
+		}
+	}
+	SpriteData.clear();
+}
---
+++ graphic/text.h
@@ -0,0 +1,41 @@
+#ifndef GRAPHIC_TEXT_H
+#define GRAPHIC_TEXT_H
+
+#include <Windows.h>
+#include <d3dx9.h>
+#include <dxerr.h>
+#include <string>
+#include <vector>
+#include "loadtex.h"
+
+class TextRenderer {
+private:
+	struct StringAttr;
+	struct SpriteAttr;
+	LPDIRECT3DDEVICE9 myDevice;
+	LPDIRECT3DTEXTURE9 font;
+	std::vector<StringAttr*> StringData;
+	std::vector<SpriteAttr*> SpriteData;
+	void reconstruct();
+	void deleteSprite();
+public:
+	TextRenderer(LPDIRECT3DDEVICE9 device);
+	~TextRenderer();
+	void NewText(unsigned int ID, const std::wstring& str, int x, int y, float scale, float width);
+	void NewText(unsigned int ID, const std::string& str, int x, int y, float scale, float width);
+	void DelText(unsigned int ID);
+	void Render();
+};
+
+struct TextRenderer::StringAttr {
+	std::wstring str;
+	int X, Y;
+	float scale, width;
+};
+
+struct TextRenderer::SpriteAttr {
+	LPD3DXSPRITE sprite;
+	int X, Y;
+	float widthScale, heightScale;
+};
+#endif
