6e3f9920f0842e028aed8da6b99b59b2d6b1addc
TANIGAWA Takashi <mihailjp@gmail.com>
2012-11-28 02:11:33 +0900

    ゲーム種別ごとの色

--- graphic/scenes/title.cpp
+++ graphic/scenes/title.cpp
@@ -1,10 +1,36 @@
 #include "title.h"
+#include "../scrmanip.h"
+#include "../gametbl.h"
 
 TitleScreen::TitleScreen(ScreenManipulator* const manipulator) : Scene(manipulator) {
+	caller = manipulator;
 }
 
 TitleScreen::~TitleScreen() {
 }
 
 void TitleScreen::Render() {
+	// バッファクリア
+	switch (GameStatus::gameStat()->gameType) {
+	case Yonma:
+		caller->getDevice()->Clear(0, nullptr, D3DCLEAR_TARGET,
+			D3DCOLOR_XRGB(0, 128, 0), 1.0f, 0);
+		break;
+	case Sanma:
+		caller->getDevice()->Clear(0, nullptr, D3DCLEAR_TARGET,
+			D3DCOLOR_XRGB(0, 0, 128), 1.0f, 0);
+		break;
+	case Sanma4:
+		caller->getDevice()->Clear(0, nullptr, D3DCLEAR_TARGET,
+			D3DCOLOR_XRGB(0, 128, 128), 1.0f, 0);
+		break;
+	case SanmaS:
+		caller->getDevice()->Clear(0, nullptr, D3DCLEAR_TARGET,
+			D3DCOLOR_XRGB(128, 0, 128), 1.0f, 0);
+		break;
+	default:
+		caller->getDevice()->Clear(0, nullptr, D3DCLEAR_TARGET,
+			D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+		break;
+	}
 }
