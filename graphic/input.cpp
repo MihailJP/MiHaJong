6234802fa70378a165ae59fb4dc7ceb8163b5212
TANIGAWA Takashi <mihailjp@gmail.com>
2012-11-29 02:43:51 +0900

    キーボード関連を一部実装

--- graphic/input.cpp
+++ graphic/input.cpp
@@ -1,1 +1,53 @@
 #include "input.h"
+#include "init.h"
+#include "../mjcore/strcode.h"
+
+namespace mihajong_graphic {
+namespace input {
+
+// -------------------------------------------------------------------------
+
+namespace {
+	InputManipulator* iManip = nullptr;
+}
+
+InputManipulator::InputManipulator(HWND hwnd) {
+	if (FAILED(DirectInput8Create(
+		GraphicDLL, 0x0800, IID_IDirectInput8, reinterpret_cast<void**>(&myInterface), nullptr)))
+		throw CodeConv::tstring(_T("DirectInput8CreateŽ¸”sII"));
+}
+
+InputManipulator::~InputManipulator() {
+	myInterface->Release();
+}
+
+// -------------------------------------------------------------------------
+
+Keyboard::Keyboard(LPDIRECTINPUT8 inputInterface, HWND hwnd) {
+	if (FAILED(inputInterface->CreateDevice(GUID_SysKeyboard, &myInputDevice, nullptr)))
+		throw CodeConv::tstring(_T("CreateDeviceŽ¸”sII"));
+	if (FAILED(myInputDevice->SetCooperativeLevel(hwnd, DISCL_NONEXCLUSIVE | DISCL_FOREGROUND)))
+		throw CodeConv::tstring(_T("SetCooperativeLevelŽ¸”sII"));
+}
+
+Keyboard::~Keyboard() {
+	if (myInputDevice) {
+		myInputDevice->Unacquire();
+		myInputDevice->Release();
+	}
+}
+
+// -------------------------------------------------------------------------
+
+EXPORT void InitInputManipulator(HWND hwnd) {
+	iManip = new InputManipulator(hwnd);
+}
+EXPORT void DisposeInputManipulator() {
+	if (iManip) delete iManip;
+	iManip = nullptr;
+}
+
+// -------------------------------------------------------------------------
+
+}
+}
--- graphic/input.h
+++ graphic/input.h
@@ -1,4 +1,36 @@
 #ifndef GRAPHIC_INPUT_H
 #define GRAPHIC_INPUT_H
 
+#include <Windows.h>
+#include <dinput.h>
+#include "exports.h"
+
+namespace mihajong_graphic {
+namespace input {
+#ifdef GRAPHIC_EXPORTS
+class InputManipulator {
+private:
+	LPDIRECTINPUT8 myInterface;
+public:
+	InputManipulator(HWND hwnd);
+	~InputManipulator();
+};
+
+class InputDevice {
+protected:
+	LPDIRECTINPUTDEVICE8 myInputDevice;
+};
+class Keyboard : public InputDevice {
+public:
+	Keyboard(LPDIRECTINPUT8 inputInterface, HWND hwnd);
+	~Keyboard();
+};
+#endif
+
+EXPORT void InitInputManipulator(HWND hwnd);
+EXPORT void DisposeInputManipulator();
+
+}
+}
+
 #endif
