8ed65dffc63753323fcfc3a04aad1e889837fea4
TANIGAWA Takashi <mihailjp@gmail.com>
2012-12-23 00:06:02 +0900

    DiceRendererクラスを除去

--- graphic/scenes/game.h
+++ graphic/scenes/game.h
@@ -10,8 +10,6 @@ namespace mihajong_graphic {
 class TableProtoScene : public Scene {
 protected:
 	ShowTile* TileTexture;
-	class DiceRenderer;
-	DiceRenderer* myDiceRenderer;
 	LPDIRECT3DTEXTURE9 tSideBar; LPD3DXSPRITE sSideBar; // サイドバー
 	void LoadTexture(LPDIRECT3DTEXTURE9* texture, LPCTSTR resource, unsigned width, unsigned height); // テクスチャ読み込み
 	void InitSprite(LPD3DXSPRITE* sprite); // スプライト初期化
@@ -20,24 +18,6 @@ protected:
 public:
 	TableProtoScene(ScreenManipulator* const manipulator);
 	virtual ~TableProtoScene();
-};
-
-class TableProtoScene::DiceRenderer {
-private:
-	static const unsigned TextureWidth = 156u;
-	static const unsigned TextureHeight = 144u;
-	static const unsigned TextureCharWidth = 24u;
-	static const unsigned TextureCharHeight = 34u;
-	static const unsigned TextureCharPadding = 2u;
-	static const signed TextureCharCenterX = 12;
-	static const signed TextureCharCenterY = 12;
-	LPDIRECT3DDEVICE9 myDevice;
-	LPDIRECT3DTEXTURE9 DiceTexture;
-	std::array<std::tuple<LPD3DXSPRITE, int, int>, 2> mySprite;
-public:
-	void Render();
-	explicit DiceRenderer(LPDIRECT3DDEVICE9 device);
-	~DiceRenderer();
 };
 
 }
