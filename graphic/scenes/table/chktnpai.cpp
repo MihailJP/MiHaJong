07a81a08f5da579e66e24fd29955a279356e4c43
TANIGAWA Takashi <mihailjp@gmail.com>
2013-01-06 18:51:36 +0900

    とりあえず聴牌かどうか表示できるところまで

--- graphic/scenes/table/chktnpai.cpp
+++ graphic/scenes/table/chktnpai.cpp
@@ -1,4 +1,6 @@
 #include "chktnpai.h"
+#include "../../gametbl.h"
+#include "../../utils.h"
 
 namespace mihajong_graphic {
 
@@ -8,7 +10,19 @@ TableSubsceneCheckTenpai::TableSubsceneCheckTenpai(LPDIRECT3DDEVICE9 device) : T
 TableSubsceneCheckTenpai::~TableSubsceneCheckTenpai() {
 }
 
+void TableSubsceneCheckTenpai::ShowTenpaiFlag(PLAYER_ID player, int x, int y) {
+	bool tenpaiflag = utils::isTenpai(GameStatus::gameStat(), player);
+	if (tenpaiflag) ShowCallMsg(player, calltext::Tenpai, x, y); // ’®”v
+	else if (GameStatus::gameStat()->Player.val[player].RichiFlag.RichiFlag)
+		ShowCallMsg(player, calltext::Chonbo, x, y); // ƒm[ƒeƒ“ƒŠ[ƒ`‚µ‚Ä‚½ê‡‚Íö˜a‚Æ•\Ž¦
+	else ShowCallMsg(player, calltext::Noten, x, y); // •s’®
+}
+
 void TableSubsceneCheckTenpai::Render() {
+	ShowTenpaiFlag(utils::RelativePositionOf(GameStatus::gameStat()->PlayerID, sOpposite), TableSize / 2      ,                 192);
+	ShowTenpaiFlag(utils::RelativePositionOf(GameStatus::gameStat()->PlayerID, sLeft    ),                 256, TableSize / 2      );
+	ShowTenpaiFlag(utils::RelativePositionOf(GameStatus::gameStat()->PlayerID, sRight   ), TableSize     - 256, TableSize / 2      );
+	ShowTenpaiFlag(utils::RelativePositionOf(GameStatus::gameStat()->PlayerID, sSelf    ), TableSize / 2      , TableSize     - 192);
 }
 
 }
--- graphic/scenes/table/chktnpai.h
+++ graphic/scenes/table/chktnpai.h
@@ -5,6 +5,8 @@
 namespace mihajong_graphic {
 	
 class TableSubsceneCheckTenpai : public TableSubsceneCallFadeProto {
+private:
+	void ShowTenpaiFlag(PLAYER_ID player, int x, int y);
 public:
 	TableSubsceneCheckTenpai(LPDIRECT3DDEVICE9 device);
 	~TableSubsceneCheckTenpai();
