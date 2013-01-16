621a44de551ebeeb996907cfb0984558682514b8
TANIGAWA Takashi <mihailjp@gmail.com>
2013-01-17 00:18:03 +0900

    サブシーンの定義追加

--- graphic/scenes/scene_id.h
+++ graphic/scenes/scene_id.h
@@ -22,6 +22,11 @@ enum TableSubsceneID : unsigned int { // 卓画面のサブシーン番号
 	tblSubsceneCallFade, // 発声表示画面(フェード)
 	tblSubsceneCallVal, // 点数の増分表示
 	tblSubsceneCallValNotenBappu, // 点数の増分表示（ノーテン罰符）
+	tblSubsceneCallValAgariten, // 点数の増分表示（和了り点）
+	tblSubsceneCallValTsumibou, // 点数の増分表示（積み棒）
+	tblSubsceneCallValChip, // 点数の増分表示（チップ）
+	tblSubsceneCallValKyoutaku, // 点数の増分表示（供託）
+	tblSubsceneCallValChonboBappu, // 点数の増分表示（錯和罰符）
 	tblSubsceneChkTenpai, // 流局時の聴牌確認
 };
 
