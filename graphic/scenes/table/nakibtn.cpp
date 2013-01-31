8368fe5d4e28b6079e47a2f9ef86c9cf93157dd0
TANIGAWA Takashi <mihailjp@gmail.com>
2013-02-01 01:51:50 +0900

    ダミーのデータでボタンを表示

--- graphic/scenes/table/nakibtn.cpp
+++ graphic/scenes/table/nakibtn.cpp
@@ -2,14 +2,32 @@
 
 namespace mihajong_graphic {
 
+const GameTableScreen::ButtonReconst::BtnData
+	GameTableScreen::ButtonReconst::buttonDat[GameTableScreen::ButtonReconst::btnMAXIMUM] = {
+		{_T(""), 0, 0, 20, 20, 0xffffffff},
+		{_T(""), 0, 0, 20, 20, 0xffffffff},
+		{_T(""), 0, 0, 20, 20, 0xffffffff},
+		{_T(""), 0, 0, 20, 20, 0xffffffff},
+		{_T(""), 0, 0, 20, 20, 0xffffffff},
+		{_T(""), 0, 0, 20, 20, 0xffffffff},
+		{_T(""), 0, 0, 20, 20, 0xffffffff},
+};
+
 void GameTableScreen::ButtonReconst::Render() {
+	buttons->Render();
 }
 
 GameTableScreen::ButtonReconst::ButtonReconst(GameTableScreen* parent) {
 	caller = parent;
+	buttons = new ButtonPic(caller->caller->getDevice());
+	for (unsigned i = 0; i < btnMAXIMUM; ++i)
+		buttons->setButton(i, ButtonPic::clear,
+		buttonDat[i].x, buttonDat[i].y, buttonDat[i].width, buttonDat[i].height,
+		buttonDat[i].color, buttonDat[i].label);
 }
 
 GameTableScreen::ButtonReconst::~ButtonReconst() {
+	delete buttons;
 }
 
 }
--- graphic/scenes/table/nakibtn.h
+++ graphic/scenes/table/nakibtn.h
@@ -1,12 +1,27 @@
 #pragma once
 
 #include "../table.h"
+#include "../../button.h"
 
 namespace mihajong_graphic {
 
 class GameTableScreen::ButtonReconst {
 private:
 	GameTableScreen* caller;
+	ButtonPic* buttons;
+public:
+	enum ButtonID {
+		btnChii, btnPon, btnKan, btnFlower, btnRiichi, btnAgari, btnPass,
+		btnMAXIMUM,
+	};
+private:
+	struct BtnData { // É{É^ÉìÇÃëÆê´äiî[
+		LPCTSTR label;
+		const int x, y;
+		const unsigned width, height;
+		const D3DCOLOR color;
+	};
+	static const BtnData buttonDat[btnMAXIMUM];
 public:
 	void Render();
 	explicit ButtonReconst(GameTableScreen* parent);
