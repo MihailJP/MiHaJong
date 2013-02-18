ce9d6282236140d5317a268ae25e47acb74e8c53
TANIGAWA Takashi <mihailjp@gmail.com>
2013-02-18 18:55:20 +0900

    構造体のメンバを並べ替え

--- mjcore/gametbl.h
+++ mjcore/gametbl.h
@@ -218,24 +218,24 @@ enum handStatCode : int8_t {
 EXPORT_STRUCT PlayerTable { // プレイヤーの状態を格納
 	LargeNum PlayerScore;
 	int playerChip; // チップの収支
-	bool SumaroFlag; // 四馬路解禁フラグ
-	bool YakitoriFlag; // 焼き鳥フラグ
 	HAND_TILES Hand; // 手牌の配列
 	DISCARD_BUF Discard; // 捨牌の配列
-	uint8_t DiscardPointer;
 	MELD_BUF Meld; // 鳴き面子を格納
+	uint8_t DiscardPointer;
 	uint8_t MeldPointer;
 	bool MenzenFlag; // 門前フラグ
 	handStatCode HandStat; // 手牌の状態（立てる・見せる・伏せる）
 	int8_t NumberOfQuads; // 槓子の数（四槓流局、三槓子、四槓子などの判定に使う）
-	RichiStat RichiFlag; // リーチしているかどうか
 	bool FirstDrawFlag; // １巡目である（地和、ダブル立直の判定に使う）
 	bool DoujunFuriten; // 同順振聴である
 	bool AgariHouki; // 和了り放棄の罰則中かどうか
+	RichiStat RichiFlag; // リーチしているかどうか
+	DECLFLAG DeclarationFlag; // 鳴きの宣言
 	FLOWERS FlowerFlag; // 晒している花牌を格納するフラグ
 	uint8_t NorthFlag; // 晒している北風牌を格納するフラグ
+	bool SumaroFlag; // 四馬路解禁フラグ
+	bool YakitoriFlag; // 焼き鳥フラグ
 	bool ConnectionLost;
-	DECLFLAG DeclarationFlag; // 鳴きの宣言
 };
 #ifdef MJCORE_EXPORTS
 static_assert(std::is_pod<PlayerTable>::value, "PlayerTable is not POD");
@@ -246,39 +246,39 @@ static_assert(std::is_pod<PlayerTable>::value, "PlayerTable is not POD");
 EXPORT_TEMPLATE_STRUCT InfoByPlayer<PlayerTable>;
 typedef InfoByPlayer<PlayerTable> StatusByPlayer;
 EXPORT_STRUCT GameTable { // 卓の情報を格納する
-	gameTypeID gameType;
 	StatusByPlayer Player;
-	PLAYER_ID PlayerID;
+	DeckBuf Deck; // 壁牌の配列
+	doraStatBook DoraFlag; // ドラ判定の配列
+	FlagByTile OpenRichiWait; // プンリーの待ち牌(ＣＯＭに意図的な放銃を起こさせないために使用)
+	paoStatBook PaoFlag; // 包フラグ（-1…なし、0～3…該当プレイヤー）
 	int GameLength;
 	int GameRound;
 	int LoopRound;
 	int Honba;
 	int Deposit;
 	int AgariChain;
+	CURRPLAYER CurrentPlayer;
+	PLAYER_ID PlayerID;
 	PLAYER_ID LastAgariPlayer;
-	FlagByTile OpenRichiWait; // プンリーの待ち牌(ＣＯＭに意図的な放銃を起こさせないために使用)
+	gameTypeID gameType;
+	bool TsumoAgariFlag; // ツモアガリ？
+	int16_t AgariSpecialStat; // 今のところ食い変えでチョンボになる場合だけ使ってる？
+	prevMeldBook PreviousMeld; // 先ほど鳴いた牌（喰い替えの判定に使う）
 	KANGSTAT KangFlag; // 嶺上開花；連開花と槓振り；頭槓和；搶槓の判定に使う
+	DICE Dice[2]; // サイコロ
 	uint8_t TurnRound; // 現在の巡目
 	uint8_t KangNum; // 四槓流局、四槓子などの判定に使う
 	bool RichiCounter; // リーチをカウンター(宣言牌をロン)
 	bool DoubleRichiCounter; // ダブルリーチをカウンター(宣言牌をロン)
 	PLAYER_ID WaremePlayer; // 割れ目の位置(-1で割れ目なし)
 	PLAYER_ID DoukasenPlayer; // 導火線の位置(-1で導火線なし)
-	DICE Dice[2]; // サイコロ
-	paoStatBook PaoFlag; // 包フラグ（-1…なし、0～3…該当プレイヤー）
-	DeckBuf Deck; // 壁牌の配列
 	uint8_t DeadTiles; // 王牌の数
 	uint8_t ExtraRinshan; // 追加の嶺上牌の数
 	bool ShibariFlag; //二飜縛り
-	doraStatBook DoraFlag; // ドラ判定の配列
 	uint8_t TilePointer; // ツモ牌のポインタ
 	uint16_t DoraPointer;
 	uint8_t RinshanPointer; // 嶺上牌のポインタ
 	bool TianHuFlag; // 親の第一打牌がまだ（天和の判定などに使う）
-	prevMeldBook PreviousMeld; // 先ほど鳴いた牌（喰い替えの判定に使う）
-	CURRPLAYER CurrentPlayer;
-	bool TsumoAgariFlag; // ツモアガリ？
-	int16_t AgariSpecialStat; // 今のところ食い変えでチョンボになる場合だけ使ってる？
 	TILE CurrentDiscard;
 };
 #ifdef MJCORE_EXPORTS
