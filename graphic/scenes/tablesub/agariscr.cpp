6356ea2b87fc17571ff04bc50d27f5017100dccf
TANIGAWA Takashi <mihailjp@gmail.com>
2013-02-20 12:08:24 +0900

    とりあえずウィンドウを表示するところまで

--- graphic/scenes/tablesub/agariscr.cpp
+++ graphic/scenes/tablesub/agariscr.cpp
@@ -1,12 +1,30 @@
 #include "agariscr.h"
+#include "../../loadtex.h"
+#include "../../resource.h"
+#include "../../sprite.h"
+#include "../../geometry.h"
+#include <cmath>
 
 namespace mihajong_graphic {
 
 // -------------------------------------------------------------------------
 
 TableSubsceneAgariScreenProto::TableSubsceneAgariScreenProto(LPDIRECT3DDEVICE9 device) : TableSubscene(device) {
+	myDevice = device;
+	LoadTexture(device, &windowTexture, MAKEINTRESOURCE(IDB_PNG_AGARI_WINDOW), 600, 864);
 }
 TableSubsceneAgariScreenProto::~TableSubsceneAgariScreenProto() {
+	windowTexture->Release();
+}
+
+void TableSubsceneAgariScreenProto::renderWindow() {
+	const double Zeit = (double)(currTime() - startTime) / 10000000.0;
+	const int yOffset = (Zeit >= 1.0) ? 0 : (int)(pow(1.0 - Zeit, 2) * (double)Geometry::BaseSize);
+	SpriteRenderer::instantiate(myDevice)->ShowSprite(
+		windowTexture,
+		((signed)Geometry::BaseSize - 600) / 2,
+		((signed)Geometry::BaseSize - 864) / 2 - yOffset,
+		600, 864);
 }
 
 // -------------------------------------------------------------------------
@@ -16,6 +34,7 @@ TableSubsceneAgariScreen::TableSubsceneAgariScreen(LPDIRECT3DDEVICE9 device) : T
 TableSubsceneAgariScreen::~TableSubsceneAgariScreen() {
 }
 void TableSubsceneAgariScreen::Render() {
+	renderWindow();
 }
 
 // -------------------------------------------------------------------------
--- graphic/scenes/tablesub/agariscr.h
+++ graphic/scenes/tablesub/agariscr.h
@@ -5,6 +5,11 @@
 namespace mihajong_graphic {
 
 class TableSubsceneAgariScreenProto : public TableSubscene {
+protected:
+	LPDIRECT3DDEVICE9 myDevice;
+	LPDIRECT3DTEXTURE9 windowTexture;
+protected:
+	void renderWindow();
 public:
 	TableSubsceneAgariScreenProto(LPDIRECT3DDEVICE9 device);
 	virtual ~TableSubsceneAgariScreenProto();
