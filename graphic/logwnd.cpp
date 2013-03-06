166b4d0d4a1b681d175b9f53e34b4ed8ac3b4224
TANIGAWA Takashi <mihailjp@gmail.com>
2013-03-07 00:54:11 +0900

    行の途中で折り返すと最後の文字が2回表示されることがあるのを修正

--- graphic/logwnd.cpp
+++ graphic/logwnd.cpp
@@ -118,7 +118,7 @@ void LogWindow::reconstruct_lines() { // çsÇ…ï™äÑ
 				**  Ç¢ÇÎÇÕÇ…ÇŸÇ÷Ç∆ÇøÇËÇ ÇÈ
 				**  0 1 2 3 4 5 6 7 8 9 10 */
 				lines.push_back(tmplog.substr(startPos, i - startPos + 1));
-				startPos = i; currentDigit = (isFullWidth(tmplog[i]) ? 2 : 1);
+				startPos = i + 1; currentDigit = (isFullWidth(tmplog[i]) ? 2 : 1);
 			}
 		}
 	}
