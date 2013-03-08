c3048622d3786fae1649f905a1f574df9382a5a4
TANIGAWA Takashi <mihailjp@gmail.com>
2013-03-08 23:28:42 +0900

    ちょっと作る

--- graphic/scenes/result.cpp
+++ graphic/scenes/result.cpp
@@ -5,18 +5,35 @@ namespace mihajong_graphic {
 // -------------------------------------------------------------------------
 
 ResultScreen::ResultScreen(ScreenManipulator* const manipulator) : SystemScreen(manipulator) {
+	rankRenderer.fill(nullptr);
 }
 
 ResultScreen::~ResultScreen() {
+	for (auto k = rankRenderer.begin(); k != rankRenderer.end(); ++k)
+		if (*k) delete *k;
 }
 
 void ResultScreen::Render() {
+	clearWithGameTypeColor();
 }
 
 void ResultScreen::KeyboardInput(LPDIDEVICEOBJECTDATA od) {
 }
 
 void ResultScreen::MouseInput(LPDIDEVICEOBJECTDATA od, int X, int Y) {
+}
+
+// -------------------------------------------------------------------------
+
+ResultScreen::RankRenderer::RankRenderer(LPDIRECT3DDEVICE9 device, int id) {
+	myDevice = device;
+	myID = id;
+}
+
+ResultScreen::RankRenderer::~RankRenderer() {
+}
+
+void ResultScreen::RankRenderer::Render() {
 }
 
 // -------------------------------------------------------------------------
--- graphic/scenes/result.h
+++ graphic/scenes/result.h
@@ -1,16 +1,32 @@
 #pragma once
 
+#include <array>
 #include "sysscr.h"
+#include "../../common/gametbl.h"
 
 namespace mihajong_graphic {
 
 class ResultScreen : public SystemScreen {
+private:
+	class RankRenderer;
+	std::array<RankRenderer*, mihajong_structs::Players> rankRenderer;
 public:
 	ResultScreen(ScreenManipulator* const manipulator);
 	~ResultScreen();
 	void Render();
 	virtual void KeyboardInput(LPDIDEVICEOBJECTDATA od);
 	virtual void MouseInput(LPDIDEVICEOBJECTDATA od, int X, int Y);
+};
+
+class ResultScreen::RankRenderer {
+private:
+	LPDIRECT3DDEVICE9 myDevice;
+	uint64_t startTime;
+	int myID;
+public:
+	RankRenderer(LPDIRECT3DDEVICE9 device, int id);
+	~RankRenderer();
+	void Render();
 };
 
 }
