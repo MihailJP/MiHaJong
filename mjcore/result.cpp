6e9699731acdc4373cefec9a8f1f3b663d1a4845
TANIGAWA Takashi <mihailjp@gmail.com>
2013-03-16 13:06:56 +0900

    BGMの停止(仮)

--- mjcore/result.cpp
+++ mjcore/result.cpp
@@ -1,7 +1,10 @@
 #include "result.h"
 #include "../graphic/graphic.h"
+#include "../sound/sound.h"
+#include "sound.h"
 
 void gameResult(const GameTable* gameStat, int origTurn, int origHonba) {
+	sound::util::bgmstop();
 	mihajong_graphic::Transit(mihajong_graphic::sceneResult);
 	/* TODO: I—¹‚Ìˆ— */
 	/* TODO: “ü—Í‘Ò‚¿
