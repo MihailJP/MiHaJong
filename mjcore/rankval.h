a2a821f9afccf9f40b93f7e91ce98d8af39d807a
TANIGAWA Takashi <mihailjp@gmail.com>
2013-03-17 15:26:36 +0900

    ウマ算出用オブジェクトを追加

---
+++ mjcore/rankval.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <array>
+#include "gametbl.h"
+
+class RankVal {
+private: /* Singleton class idiom */
+	RankVal(); // INTERNAL USE ONLY
+	RankVal(const RankVal&); // DO NOT USE
+	RankVal& operator=(const RankVal&); // DO NOT USE
+private:
+	typedef std::array<std::array<int, Players - 1>, Players + 1> RankValSet;
+	std::map<std::string, RankValSet> rankValueMap; // ƒEƒ}İ’èˆê——
+public:
+	static RankVal* Instantiate(); // Singleton instance accessor
+	int getRankVal(const GameTable* gameStat, const std::string& ruletag, unsigned playersAboveBase, unsigned rank) const;
+};
