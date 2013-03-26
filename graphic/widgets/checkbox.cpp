f5672b7d1f19597fa661b252d9f2f14cca81a10a
TANIGAWA Takashi <mihailjp@gmail.com>
2013-03-26 21:41:10 +0900

    コンストラクタでのキャプション設定

--- graphic/widgets/checkbox.cpp
+++ graphic/widgets/checkbox.cpp
@@ -4,12 +4,15 @@
 
 namespace mihajong_graphic {
 
-CheckBox::CheckBox(LPDIRECT3DDEVICE9 device) {
+CheckBox::CheckBox(LPDIRECT3DDEVICE9 device, const CodeConv::tstring& caption) {
 	myDevice = device;
 	LoadTexture(device, &myTexture, MAKEINTRESOURCE(IDB_PNG_CHECKBOX), 72, 72);
+	myTextRenderer = new TextRenderer(device);
+	myCaption = caption;
 }
 
 CheckBox::~CheckBox() {
+	delete myTextRenderer;
 	if (myTexture) myTexture->Release();
 }
 
--- graphic/widgets/checkbox.h
+++ graphic/widgets/checkbox.h
@@ -2,7 +2,8 @@
 
 #include <d3dx9.h>
 #include <dxerr.h>
-
+#include "../../common/strcode.h"
+#include "../text.h"
 
 namespace mihajong_graphic {
 
@@ -10,9 +11,13 @@ class CheckBox {
 private:
 	LPDIRECT3DTEXTURE9 myTexture;
 	LPDIRECT3DDEVICE9 myDevice;
+	TextRenderer* myTextRenderer;
+	CodeConv::tstring myCaption;
 public:
-	explicit CheckBox(LPDIRECT3DDEVICE9 device);
+	explicit CheckBox(LPDIRECT3DDEVICE9 device, const CodeConv::tstring& caption = _T("foobar"));
 	~CheckBox();
+	const size_t captionWidth() {return myTextRenderer->strWidthByCols(myCaption);}
+	const size_t captionWidthPx() {return myTextRenderer->strWidthByPix(myCaption);}
 };
 
 }
