c942655b09f9bd538aaa02ac1ab20a01437c34e9
TANIGAWA Takashi <mihailjp@gmail.com>
2013-03-26 20:08:51 +0900

    チェックボックスの画像を追加

--- graphic/widgets/checkbox.cpp
+++ graphic/widgets/checkbox.cpp
@@ -1,12 +1,16 @@
 #include "checkbox.h"
+#include "../loadtex.h"
+#include "../resource.h"
 
 namespace mihajong_graphic {
 
 CheckBox::CheckBox(LPDIRECT3DDEVICE9 device) {
 	myDevice = device;
+	LoadTexture(device, &myTexture, MAKEINTRESOURCE(IDB_PNG_CHECKBOX), 72, 72);
 }
 
 CheckBox::~CheckBox() {
+	if (myTexture) myTexture->Release();
 }
 
 }
--- graphic/widgets/checkbox.h
+++ graphic/widgets/checkbox.h
@@ -3,10 +3,12 @@
 #include <d3dx9.h>
 #include <dxerr.h>
 
+
 namespace mihajong_graphic {
 
 class CheckBox {
 private:
+	LPDIRECT3DTEXTURE9 myTexture;
 	LPDIRECT3DDEVICE9 myDevice;
 public:
 	explicit CheckBox(LPDIRECT3DDEVICE9 device);
