41e1297ffc649692b94d731756a4985b83308d73
TANIGAWA Takashi <mihailjp@gmail.com>
2013-04-18 23:40:00 +0900

    init.cpp暫定処理

--- mjcore/init.h
+++ mjcore/init.h
@@ -3,4 +3,8 @@
 #include "mjexport.h"
 #include "gametbl.h"
 
+#ifdef _WIN32
 MJCORE void initapp(GameTypeID gameType, HWND hwnd);
+#else /*_WIN32*/
+MJCORE void initapp(GameTypeID gameType, void* hwnd);
+#endif /*_WIN32*/
