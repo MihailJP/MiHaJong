2f072620c8c18210df67a2f87accabe12e7938ff
TANIGAWA Takashi <mihailjp@gmail.com>
2013-04-18 23:45:23 +0900

    main.cpp暫定処理

--- mjcore/main.cpp
+++ mjcore/main.cpp
@@ -9,24 +9,35 @@
 
 GameThread* gameThread = nullptr;
 
+#ifdef _WIN32
 GameThread::GameThread(GameTypeID gameType, HWND hwnd) {
 	myGameType = gameType;
 	hWnd = hwnd;
 	hThread = CreateThread(nullptr, 0, ThreadMain, this, 0, nullptr);
 }
+#else /*_WIN32*/
+GameThread::GameThread(GameTypeID gameType, void* hwnd) {
+	/* TODO: 未実装箇所 */
+}
+#endif /*_WIN32*/
 
 GameThread::~GameThread() {
 	/* WE ASSUME THAT THIS DESTRUCTOR GETS CALLED ONLY WHEN THE PROGRAM IS ABOUT TO END. */
 	/* DO NOT CALL THIS DESTRUCTOR ELSEWHEN, OR IT MAY CAUSE SOME LEAKS!!! */
 	/* プログラム終了時にのみ呼び出されることを想定しています */
 	/* それ以外の時には絶対に呼び出さないこと！！！ */
+#ifdef _WIN32
 	DWORD exitCode; GetExitCodeThread(hThread, &exitCode);
 	if (exitCode == STILL_ACTIVE) {
 		warn(_T("スレッドを強制終了します！"));
 		TerminateThread(hThread, S_OK);
 	}
+#else /*_WIN32*/
+	/* TODO: 未実装箇所 */
+#endif /*_WIN32*/
 }
 
+#ifdef _WIN32
 DWORD WINAPI GameThread::ThreadMain(LPVOID lpParam) {
 	GameTypeID gameType = reinterpret_cast<GameThread*>(lpParam)->myGameType;
 	HWND hwnd = reinterpret_cast<GameThread*>(lpParam)->hWnd;
@@ -36,10 +47,20 @@ DWORD WINAPI GameThread::ThreadMain(LPVOID lpParam) {
 	SendMessage(hwnd, WM_CLOSE, 0, 0);
 	return S_OK;
 }
+#else /*_WIN32*/
+/* TODO: 未実装箇所 */
+#endif /*_WIN32*/
 
+#ifdef _WIN32
 MJCORE void StartGame (GameTypeID gameType, HWND hwnd) {
 	gameThread = new GameThread(gameType, hwnd);
 }
+#else /*_WIN32*/
+MJCORE void StartGame (GameTypeID gameType, void* hwnd) {
+	/* TODO: 暫定実装 */
+	gameThread = new GameThread(gameType, nullptr);
+}
+#endif /*_WIN32*/
 
 MJCORE void TerminateGame () {
 	delete gameThread;
--- mjcore/main.h
+++ mjcore/main.h
@@ -2,23 +2,37 @@
 
 #include "mjexport.h"
 #include "gametbl.h"
+#ifdef _WIN32
 #include <windows.h>
+#endif /*_WIN32*/
 
 #ifdef MJCORE_EXPORTS
 
 class GameThread {
 private:
 	GameTypeID myGameType;
+#ifdef _WIN32
 	HANDLE hThread;
 	HWND hWnd;
 	static DWORD WINAPI ThreadMain(LPVOID lpParam);
+#else /*_WIN32*/
+	/* TODO: ñ¢é¿ëïâ”èä */
+#endif /*_WIN32*/
 public:
+#ifdef _WIN32
 	explicit GameThread(GameTypeID gameType, HWND hWnd);
+#else /*_WIN32*/
+	explicit GameThread(GameTypeID gameType, void* hWnd); // TODO: ébíË
+#endif /*_WIN32*/
 	~GameThread();
 };
 
 extern GameThread* gameThread;
 #endif
 
+#ifdef _WIN32
 MJCORE void StartGame (GameTypeID gameType, HWND hwnd);
+#else /*_WIN32*/
+MJCORE void StartGame (GameTypeID gameType, void* hwnd); // TODO: ébíË
+#endif /*_WIN32*/
 MJCORE void TerminateGame ();
