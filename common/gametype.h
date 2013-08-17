458cee71f92c1317f2aff50d5f71f6cda7aa36e5
TANIGAWA Takashi <mihailjp@gmail.com>
2013-08-17 22:22:22 +0900

    中国ルール用の実行ファイルを用意

--- common/gametype.h
+++ common/gametype.h
@@ -8,7 +8,8 @@ using std::uint8_t;
 
 enum GameTypeID : uint8_t { // 卓の種類(四麻、三麻)指定用
 	Yonma = 0x01, Sanma = 0x02, Sanma4 = 0x04, SanmaS = 0x08,
-	AllSanma = 0x0e, SanmaT = 0x0a, SanmaX = 0x06
+	AllSanma = 0x0e, SanmaT = 0x0a, SanmaX = 0x06, RichiMJ = 0x0f,
+	GuobiaoMJ = 0x10,
 };
 
 } /* namespace */
