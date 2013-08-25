86bf9e9e01eb33caadeeacbbb604aee7cc9cbc9d
TANIGAWA Takashi <mihailjp@gmail.com>
2013-08-25 10:11:12 +0900

    花牌処理

--- data/gbai/default.lua
+++ data/gbai/default.lua
@@ -1,6 +1,22 @@
 -- MiHaJong中国麻雀 デフォルトAI
 
+function isflower (tile) -- 花牌かどうか判定
+	if tile then -- nilだったらエラーになるのでそれを防止
+		for k, v in getiter(mihajong.Tile.Flower) do
+			if tile.tile == v then return true end
+		end
+	end
+	return false
+end
+
 function ontsumo (gametbl) -- ＡＩの打牌
+	local haiHand = gametbl:gethand()
+
+	-- 花牌を抜く
+	for cnt = 1, 14 do
+		if isflower(haiHand[cnt]) then return mihajong.DiscardType.Flower, cnt end 
+	end
+
 	return mihajong.DiscardType.Normal, 14
 end
 
