ccc63b12570d764c1c9a46f618158cc0b84ddfde
MihailJP <mihailjp@gmail.com>
2019-03-21 16:19:56 +0900

    tilecode.hにコメントを付ける

--- common/tilecode.h
+++ common/tilecode.h
@@ -11,75 +11,75 @@
 namespace mihajong_structs {
 
 // 牌の種類
-constexpr unsigned int TileCodeMaximum    = 200;
-constexpr unsigned int TileSuitStep       =  10;
-constexpr unsigned int TileNumeralColors  =   3;
+constexpr unsigned int TileCodeMaximum    = 200; // 牌コード最大値
+constexpr unsigned int TileSuitStep       =  10; // 色ごとの種類数
+constexpr unsigned int TileNumeralColors  =   3; // 数牌の色の種類
 
 enum TileSuit : uint8_t {
-	TileSuitInvalid    = 0xffu,
-	TileSuitCharacters = TileSuitStep * 0,
-	TileSuitCircles    = TileSuitStep * 1,
-	TileSuitBamboos    = TileSuitStep * 2,
-	TileSuitHonors     = TileSuitStep * TileNumeralColors,
-	TileSuitFlowers    = TileSuitStep * 12
+	TileSuitInvalid    = 0xffu,                            /* 無効データ */
+	TileSuitCharacters = TileSuitStep * 0,                 /* 萬子 */
+	TileSuitCircles    = TileSuitStep * 1,                 /* 筒子 */
+	TileSuitBamboos    = TileSuitStep * 2,                 /* 索子 */
+	TileSuitHonors     = TileSuitStep * TileNumeralColors, /* 字牌 */
+	TileSuitFlowers    = TileSuitStep * 12,                /* 花牌 */
 };
 
-constexpr unsigned int TileNonflowerMax = TileSuitStep + TileSuitHonors;
-constexpr unsigned int TileFlowerMax    = TileSuitStep + TileSuitFlowers;
+constexpr unsigned int TileNonflowerMax = TileSuitStep + TileSuitHonors;  // 普通牌の最大値
+constexpr unsigned int TileFlowerMax    = TileSuitStep + TileSuitFlowers; // 花牌の最大値
 
 enum TileCode : uint8_t { // 牌のコード
-	NoTile = 0,
+	NoTile = 0,                            // 牌なし
 	/* 萬子 */
-	CharacterOne = TileSuitCharacters + 1,
-	CharacterTwo,
-	CharacterThree,
-	CharacterFour,
-	CharacterFive,
-	CharacterSix,
-	CharacterSeven,
-	CharacterEight,
-	CharacterNine,
+	CharacterOne = TileSuitCharacters + 1, // 一萬
+	CharacterTwo,                          // 二萬
+	CharacterThree,                        // 三萬
+	CharacterFour,                         // 四萬
+	CharacterFive,                         // 五萬
+	CharacterSix,                          // 六萬
+	CharacterSeven,                        // 七萬
+	CharacterEight,                        // 八萬
+	CharacterNine,                         // 九萬
 	/* 筒子 */
-	CircleOne = TileSuitCircles + 1,
-	CircleTwo,
-	CircleThree,
-	CircleFour,
-	CircleFive,
-	CircleSix,
-	CircleSeven,
-	CircleEight,
-	CircleNine,
+	CircleOne = TileSuitCircles + 1,       // 一筒
+	CircleTwo,                             // 二筒
+	CircleThree,                           // 三筒
+	CircleFour,                            // 四筒
+	CircleFive,                            // 五筒
+	CircleSix,                             // 六筒
+	CircleSeven,                           // 七筒
+	CircleEight,                           // 八筒
+	CircleNine,                            // 九筒
 	/* 索子 */
-	BambooOne = TileSuitBamboos + 1,
-	BambooTwo,
-	BambooThree,
-	BambooFour,
-	BambooFive,
-	BambooSix,
-	BambooSeven,
-	BambooEight,
-	BambooNine,
+	BambooOne = TileSuitBamboos + 1,       // 一索
+	BambooTwo,                             // 二索
+	BambooThree,                           // 三索
+	BambooFour,                            // 四索
+	BambooFive,                            // 五索
+	BambooSix,                             // 六索
+	BambooSeven,                           // 七索
+	BambooEight,                           // 八索
+	BambooNine,                            // 九索
 	/* 字牌 */
-	EastWind = TileSuitHonors + 1,
-	SouthWind,
-	WestWind,
-	NorthWind,
-	WhiteDragon,
-	GreenDragon,
-	RedDragon,
+	EastWind = TileSuitHonors + 1,         // 東風牌
+	SouthWind,                             // 南風牌
+	WestWind,                              // 西風牌
+	NorthWind,                             // 北風牌
+	WhiteDragon,                           // 三元牌 白
+	GreenDragon,                           // 三元牌 發
+	RedDragon,                             // 三元牌 中
 	/* 花牌 */
-	Spring = TileSuitFlowers + 1,
-	Summer,
-	Autumn,
-	Winter,
-	Plum = TileSuitFlowers + 6,
-	Orchid,
-	Chrysanthemum,
-	Bamboo,
+	Spring = TileSuitFlowers + 1,          // 季節牌 春
+	Summer,                                // 季節牌 夏
+	Autumn,                                // 季節牌 秋
+	Winter,                                // 季節牌 冬
+	Plum = TileSuitFlowers + 6,            // 草木牌 梅
+	Orchid,                                // 草木牌 蘭
+	Chrysanthemum,                         // 草木牌 菊
+	Bamboo,                                // 草木牌 竹
 	/* 特殊 */
-	Flower = 38,
-	BackSide = 39,
-	TilePad = UCHAR_MAX,
+	Flower = 38,                           // 判定用 花牌総称
+	BackSide = 39,                         // 表示用 裏返し
+	TilePad = UCHAR_MAX,                   // 理牌用 パディング
 };
 
 // -------------------------------------------------------------------------
