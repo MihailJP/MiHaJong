acb87fa52e0c33fdfba58f98df7c41d7ec91cf22
MihailJP <mihailjp@gmail.com>
2019-03-24 11:49:23 +0900

    md2html.pyを修正
    Python3だと動かないのを修正
    .mdへのリンクを.htmlに修正

--- auxtools/md2html.py
+++ auxtools/md2html.py
@@ -10,17 +10,17 @@ sys.stdin = codecs.getreader('utf-8')(sys.stdin)
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 mdText = sys.stdin.read()
 
-print "<!DOCTYPE html>"
-print "<html>"
-print "<head>"
-print "<link rel=\"stylesheet\" href=\"manual.css\" type=\"text/css\">"
-print "<title>" + re.compile('\\r?\\n').split(mdText)[0] + "</title>"
-print "</head>"
-print "<body>"
+print("<!DOCTYPE html>")
+print("<html>")
+print("<head>")
+print("<link rel=\"stylesheet\" href=\"manual.css\" type=\"text/css\">")
+print("<title>" + re.compile('\\r?\\n').split(mdText)[0] + "</title>")
+print("</head>")
+print("<body>")
 
-print markdown.markdown(mdText,
+print(markdown.markdown(mdText.replace('.md', '.html'),
                         extensions=['tables', 'fenced_code', 'codehilite'],
-                        output_format='html5')
+                        output_format='html5'))
 
-print "</body>"
-print "</html>"
+print("</body>")
+print("</html>")
